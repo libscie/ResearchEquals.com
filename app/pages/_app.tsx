9c880ad12409c07aafa32a143f3d1f3f16ef6273
Chris Hartgerink <chris@libscie.org>
2021-08-20 13:46:57 +0200

    Init Blitz.JS with Tailwind

--- app/pages/_app.tsx
+++ app/pages/_app.tsx
@@ -9,6 +9,8 @@ import {
 } from "blitz"
 import LoginForm from "app/auth/components/LoginForm"
 
+import "app/core/styles/index.css"
+
 export default function App({ Component, pageProps }: AppProps) {
   const getLayout = Component.getLayout || ((page) => page)
 
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+// tailwind.config.js
+module.exports = {
+  mode: "jit",
+  purge: ["{pages,app}/**/*.{js,ts,jsx,tsx}"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
