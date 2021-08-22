69ab1d07790e9f35e9385824fa983eab4fa3e3a4
Chris Hartgerink <chris@libscie.org>
2021-08-22 18:25:19 +0200

    Add tailwind hero

--- app/core/layouts/Layout.tsx
+++ app/core/layouts/Layout.tsx
@@ -13,8 +13,9 @@ const Layout = ({ title, children }: LayoutProps) => {
         <title>{title || "web-app-tbd"}</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      {children}
+      <div className="w-screen h-screen bg-white dark:bg-gray-900 text-black dark:text-white">
+        {children}
+      </div>
     </>
   )
 }
--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -1,66 +1,9 @@
-import { Suspense } from "react"
-import { Link, BlitzPage, useMutation, Routes } from "blitz"
+import { BlitzPage } from "blitz"
 import Layout from "app/core/layouts/Layout"
-import { useCurrentUser } from "app/core/hooks/useCurrentUser"
-import logout from "app/auth/mutations/logout"
-
-/*
- * This file is just for a pleasant getting started page for your new app.
- * You can delete everything in here and start from scratch if you like.
- */
-
-const UserInfo = () => {
-  const currentUser = useCurrentUser()
-  const [logoutMutation] = useMutation(logout)
-
-  if (currentUser) {
-    return (
-      <>
-        <button
-          className="button small"
-          onClick={async () => {
-            await logoutMutation()
-          }}
-        >
-          Logout
-        </button>
-        <div>
-          User id: <code>{currentUser.id}</code>
-          <br />
-          User role: <code>{currentUser.role}</code>
-        </div>
-      </>
-    )
-  } else {
-    return (
-      <>
-        <Link href={Routes.SignupPage()}>
-          <a className="button small">
-            <strong>Sign Up</strong>
-          </a>
-        </Link>
-        <Link href={Routes.LoginPage()}>
-          <a className="button small">
-            <strong>Login</strong>
-          </a>
-        </Link>
-      </>
-    )
-  }
-}
+import Navbar from "../core/components/navbar"
 
 const Home: BlitzPage = () => {
-  return (
-    <div className="container">
-      <main>
-        <div className="buttons" style={{ marginTop: "1rem", marginBottom: "1rem" }}>
-          <Suspense fallback="Loading...">
-            <UserInfo />
-          </Suspense>
-        </div>
-      </main>
-    </div>
-  )
+  return <Navbar />
 }
 
 Home.suppressFirstRenderFlicker = true
