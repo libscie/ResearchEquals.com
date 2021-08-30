02949251b64797fd46720a3a9fee20c758e196d9
Chris Hartgerink <chris@libscie.org>
2021-08-30 13:25:03 +0200

    Scaffold /dashboard

---
+++ app/pages/dashboard.tsx
@@ -0,0 +1,17 @@
+import { BlitzPage } from "blitz"
+import Layout from "app/core/layouts/Layout"
+import Navbar from "../core/components/navbar"
+
+const Dashboard: BlitzPage = () => {
+  return (
+    <>
+      <Navbar />
+      <main className="lg:relative">TEST</main>
+    </>
+  )
+}
+
+Dashboard.suppressFirstRenderFlicker = true
+Dashboard.getLayout = (page) => <Layout title="Dashboard">{page}</Layout>
+
+export default Dashboard
