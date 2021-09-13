1f5f5d6b93f67ae6fabd0caebc9d4635c0532262
Chris Hartgerink <chris@libscie.org>
2021-09-13 14:06:38 +0200

    Update migrations

--- db/migrations/20210913120605_init/migration.sql
+++ db/migrations/20210913120605_init/migration.sql
@@ -1,5 +1,31 @@
 -- CreateEnum
+CREATE TYPE "MembershipRole" AS ENUM ('OWNER', 'ADMIN', 'USER');
+
+-- CreateEnum
+CREATE TYPE "GlobalRole" AS ENUM ('SUPERADMIN', 'CUSTOMER');
+
+-- CreateEnum
 CREATE TYPE "TokenType" AS ENUM ('RESET_PASSWORD');
+
+-- CreateTable
+CREATE TABLE "Workspace" (
+    "id" SERIAL NOT NULL,
+    "handle" TEXT NOT NULL,
+
+    PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Membership" (
+    "id" SERIAL NOT NULL,
+    "role" "MembershipRole" NOT NULL,
+    "workspaceId" INTEGER NOT NULL,
+    "userId" INTEGER,
+    "invitedName" TEXT,
+    "invitedEmail" TEXT,
+
+    PRIMARY KEY ("id")
+);
 
 -- CreateTable
 CREATE TABLE "User" (
@@ -8,8 +34,9 @@ CREATE TABLE "User" (
     "updatedAt" TIMESTAMP(3) NOT NULL,
     "name" TEXT,
     "email" TEXT NOT NULL,
+    "emailIsVerified" BOOLEAN NOT NULL DEFAULT false,
     "hashedPassword" TEXT,
-    "role" TEXT NOT NULL DEFAULT E'USER',
+    "role" "GlobalRole" NOT NULL,
 
     PRIMARY KEY ("id")
 );
@@ -45,6 +72,9 @@ CREATE TABLE "Token" (
 );
 
 -- CreateIndex
+CREATE UNIQUE INDEX "Membership.workspaceId_invitedEmail_unique" ON "Membership"("workspaceId", "invitedEmail");
+
+-- CreateIndex
 CREATE UNIQUE INDEX "User.email_unique" ON "User"("email");
 
 -- CreateIndex
@@ -52,6 +82,12 @@ CREATE UNIQUE INDEX "Session.handle_unique" ON "Session"("handle");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "Token.hashedToken_type_unique" ON "Token"("hashedToken", "type");
+
+-- AddForeignKey
+ALTER TABLE "Membership" ADD FOREIGN KEY ("workspaceId") REFERENCES "Workspace"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Membership" ADD FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "Session" ADD FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE SET NULL ON UPDATE CASCADE;
