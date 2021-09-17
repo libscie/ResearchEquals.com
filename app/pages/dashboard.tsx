ab000d57606a3d185fc0aef03b33f3b25201e061
Chris Hartgerink <chris@libscie.org>
2021-09-17 20:16:33 +0200

    Add avatar upload button to dashboard

--- app/pages/dashboard.tsx
+++ app/pages/dashboard.tsx
@@ -1,12 +1,36 @@
-import { BlitzPage } from "blitz"
+import { BlitzPage, useMutation, invokeWithMiddleware } from "blitz"
 import Layout from "app/core/layouts/Layout"
-import Navbar from "../core/components/navbar"
+import { Widget } from "@uploadcare/react-widget"
 
-const Dashboard: BlitzPage = () => {
+import Navbar from "../core/components/navbar"
+import updateWorkspaceAvatar from "../workspaces/mutations/updateWorkspaceAvatar"
+import getCurrentWorkspace from "app/workspaces/queries/getCurrentWorkspace"
+
+export const getServerSideProps = async ({ req, res }) => {
+  const workspace = await invokeWithMiddleware(getCurrentWorkspace, null, { req, res })
+  return { props: { workspace } }
+}
+
+const Dashboard: BlitzPage = ({ workspace }) => {
+  const [updateWorkspaceAvatarMutation] = useMutation(updateWorkspaceAvatar)
+
   return (
     <>
       <Navbar />
-      <main className="lg:relative">TEST</main>
+      <Widget
+        publicKey={process.env.UPLOADCARE_PUBLIC_KEY}
+        onChange={async (info) => {
+          try {
+            await updateWorkspaceAvatarMutation({
+              handle: workspace.handle,
+              avatar: info.cdnUrl ?? "",
+            })
+          } catch (err) {
+            alert(err)
+          }
+          console.log("Upload completed:", info)
+        }}
+      />
     </>
   )
 }
