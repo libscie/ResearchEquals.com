937c74d45e98048476d4bad4f7ff989de189d8f0
Chris Hartgerink <chris@libscie.org>
2021-09-21 16:10:37 +0200

    Flip ORCID to production

---
+++ app/api/auth/[...auth].ts
@@ -0,0 +1,36 @@
+import { passportAuth } from "blitz"
+import db from "db"
+import { Strategy as OrcidStrategy } from "passport-orcid"
+
+export default passportAuth(({ ctx, req, res }) => ({
+  successRedirectUrl: "/dashboard",
+  errorRedirectUrl: "/",
+  strategies: [
+    {
+      strategy: new OrcidStrategy(
+        // This strategy can only be used to add the ORCID to a workspace (right now)
+        // Not used to login (yet)
+        {
+          sandbox: false,
+          clientID: process.env.ORCID_CLIENT_ID,
+          clientSecret: process.env.ORCID_CLIENT_SECRET,
+          callbackURL: `${process.env.APP_ORIGIN}/api/auth/orcid/callback`,
+        },
+        async function (accessToken, refreshToken, params, profile, done) {
+          // TODO add a way for the user to know when this fails
+          // EG when another account is already linked to the orcid
+          await db.workspace.update({
+            where: {
+              id: ctx.session.$publicData.workspaceId,
+            },
+            data: {
+              orcid: params.orcid,
+            },
+          })
+
+          return done(null, { publicData: ctx.session.$publicData })
+        }
+      ),
+    },
+  ],
+}))
