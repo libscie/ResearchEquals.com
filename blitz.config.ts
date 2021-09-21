f0f0ed89240a86ac6834e70e112f4130b59bdb86
Chris Hartgerink <chris@libscie.org>
2021-09-21 13:52:42 +0200

    Add environment variables for orcid

--- blitz.config.ts
+++ blitz.config.ts
@@ -9,6 +9,8 @@ const config: BlitzConfig = {
   ],
   env: {
     UPLOADCARE_PUBLIC_KEY: process.env.UPLOADCARE_PUBLIC_KEY,
+    ORCID_CLIENT_ID: process.env.ORCID_CLIENT_ID,
+    ORCID_CLIENT_SECRET: process.env.ORCID_CLIENT_SECRET,
   },
   /* Uncomment this to customize the webpack config
   webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
