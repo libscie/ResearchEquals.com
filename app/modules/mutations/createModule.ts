963a3fbf1f393214fcb832bfc9c75199cbeb777b
Chris Hartgerink <chris@libscie.org>
2021-09-25 15:29:46 +0200

    Lint

--- app/modules/mutations/createModule.ts
+++ app/modules/mutations/createModule.ts
@@ -2,23 +2,26 @@ import { NotFoundError, resolver } from "blitz"
 import db from "db"
 import generateSuffix from "./generateSuffix"
 
-export default resolver.pipe(resolver.authorize(), async ({ title, type, main }, ctx) => {
-  // Create module
-  const module = await db.module.create({
-    data: {
-      suffix: await generateSuffix(undefined),
-      title,
-      type,
-      main,
-      authors: {
-        connect: [
-          {
-            id: ctx.session.$publicData.workspaceId,
-          },
-        ],
+export default resolver.pipe(
+  resolver.authorize(),
+  async ({ title, description, type, main }, ctx) => {
+    const module = await db.module.create({
+      data: {
+        suffix: await generateSuffix(undefined),
+        title,
+        description,
+        type,
+        main,
+        authors: {
+          connect: [
+            {
+              id: ctx.session.$publicData.workspaceId,
+            },
+          ],
+        },
       },
-    },
-  })
+    })
 
-  return true
-})
+    return true
+  }
+)
