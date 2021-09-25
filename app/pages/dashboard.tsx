c9ac83b207ebadd01f00781939a91cea8958931e
Chris Hartgerink <chris@libscie.org>
2021-09-25 15:40:07 +0200

    Update dashboard with Feed and Newest Users

--- app/pages/dashboard.tsx
+++ app/pages/dashboard.tsx
@@ -1,22 +1,75 @@
 import { BlitzPage, Link, Routes } from "blitz"
 import Layout from "app/core/layouts/Layout"
-import Navbar from "../core/components/navbar"
+import moment from "moment"
 
-const Dashboard: BlitzPage = () => {
+import Navbar from "../core/components/navbar"
+import db from "db"
+
+export const getServerSideProps = async () => {
+  const modules = await db.module.findMany()
+  const workspaces = await db.workspace.findMany({
+    orderBy: [
+      {
+        createdAt: "desc",
+      },
+    ],
+  })
+
+  return { props: { modules, workspaces } }
+}
+
+const Dashboard = ({ modules, workspaces }) => {
   return (
     <>
       <Navbar />
-      <main className="lg:relative">
-        <p>
-          <Link href={Routes.CreatePage()}>
-            <a>Lets create some shit</a>
-          </Link>
-        </p>
-        <p>
-          <Link href={Routes.SettingsPage()}>
-            <a>Go to settings</a>
-          </Link>
-        </p>
+      <main className="max-w-4xl mx-auto">
+        <div>
+          <p>
+            <Link href={Routes.CreatePage()}>
+              <a>Lets create some shit</a>
+            </Link>
+          </p>
+          <p>
+            <Link href={Routes.DraftsPage()}>
+              <a>Check yo drafts</a>
+            </Link>
+          </p>
+          <p>
+            <Link href={Routes.SettingsPage()}>
+              <a>Go to settings</a>
+            </Link>
+          </p>
+        </div>
+        <div className="flex">
+          <div className="w-full">
+            <h2 className="font-bold text-4xl">Feed</h2>
+            {modules.map((module) => {
+              return (
+                <p key={module.suffix}>
+                  <Link href={Routes.ModulePage({ suffix: module.suffix })}>
+                    <a>
+                      {module.suffix} {module.title}
+                    </a>
+                  </Link>
+                </p>
+              )
+            })}
+          </div>
+        </div>
+        <div>
+          <h2 className="font-bold text-4xl">Newest users</h2>
+          {workspaces.map((workspace) => {
+            return (
+              <p key={workspace.handle}>
+                <Link href={Routes.HandlePage({ handle: workspace.handle })}>
+                  <a>
+                    {moment(workspace.createdAt).fromNow()} @{workspace.handle}
+                  </a>
+                </Link>
+              </p>
+            )
+          })}
+        </div>
       </main>
     </>
   )
