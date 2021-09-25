170e4c1cc942ff5ae721f128fd2e937c86a1e0df
Chris Hartgerink <chris@libscie.org>
2021-09-25 17:59:20 +0200

    Update suffix access permissions

--- app/pages/modules/[suffix].tsx
+++ app/pages/modules/[suffix].tsx
@@ -1,13 +1,26 @@
+import { AuthenticationError, getSession } from "blitz"
 import Layout from "../../core/layouts/Layout"
 import db from "db"
 
-export const getServerSideProps = async ({ params }) => {
+export const getServerSideProps = async ({ params, req, res }) => {
+  const session = await getSession(req, res)
+
   const suffix = params!.suffix
   const module = await db.module.findFirst({
     where: { suffix },
+    include: {
+      authors: true,
+    },
   })
 
-  if (!module) {
+  // Throw 404 if
+  // 1. Module does not exist
+  // 2. Module exists but is unpublished and not authored by current workspace
+  if (
+    !module ||
+    (module.published === false &&
+      !(module.authors.filter((e) => e.id === session.$publicData.workspaceId).length > 0))
+  ) {
     return {
       notFound: true,
     }
@@ -18,7 +31,7 @@ export const getServerSideProps = async ({ params }) => {
 
 const ModulePage = ({ module }) => {
   return (
-    <Layout title={module.title}>
+    <Layout title={`R= ${module.title}`}>
       <div className="flex justify-center items-center">
         <h1>{module.title}</h1>
         <p>{module.description}</p>
