7ca3f4f22079b0d6c7f6f6a821ff646a7729b771
Chris Hartgerink <chris@libscie.org>
2021-10-02 23:32:19 +0200

    Add image routes for handle and suffix

---
+++ app/api/images/workspaces/[handle].ts
@@ -0,0 +1,68 @@
+import { BlitzApiRequest, BlitzApiResponse } from "blitz"
+import db from "db"
+import htmlToImage from "../../../htmlToImage"
+
+const handler = async (req: BlitzApiRequest, res: BlitzApiResponse) => {
+  const {
+    query: { handle },
+  } = req
+
+  const module = await db.workspace.findFirst({ where: { handle } })
+  const imageBuffer = await htmlToImage(
+    `
+      <html>
+        <head>
+          <style>
+            * {
+              margin: 0;
+              padding: 0;
+            }
+
+            *,
+            *:before,
+            *:after {
+              box-sizing: border-box;
+            }
+
+            html,
+            body {
+              background: #0099ff;
+              width: 1200px;
+              height: 628px;
+              font-family: "Helvetica Neue", "Helvetica", "Arial", sans-serif;
+            }
+
+            div {
+              width: 1200px;
+              height: 628px;
+              padding: 0 200px;
+              display: flex;
+              align-items: center;
+              justify-content: center;
+            }
+
+            h1 {
+              font-size: 48px;
+              line-height: 56px;
+              color: #fff;
+              margin: 0;
+              text-align: center;
+            }
+          </style>
+        </head>
+        <body>
+          <div>
+            <h1>${handle}</h1>
+            <p>${handle.name}</p>
+          </div>
+        </body>
+      </html>
+    `
+  )
+
+  res.statusCode = 200
+
+  res.setHeader("Content-Type", "image/png")
+  res.end(imageBuffer)
+}
+export default handler
