ce01d6fd52260b193ba326b60e26b202d562a923
Chris Hartgerink <chris@libscie.org>
2021-10-02 23:32:01 +0200

    Add image constructor

---
+++ app/htmlToImage.ts
@@ -0,0 +1,18 @@
+import puppeteer from "puppeteer"
+
+const htmlToImage = async (html = "") => {
+  const browser = await puppeteer.launch()
+  const page = await browser.newPage()
+
+  await page.setContent(html)
+
+  const content = await page.$("body")
+  const imageBuffer = await content.screenshot({ omitBackground: true })
+
+  await page.close()
+  await browser.close()
+
+  return imageBuffer
+}
+
+export default htmlToImage
