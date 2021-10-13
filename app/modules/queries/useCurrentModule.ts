eaea991ac55486d7eebf18397c827f190f9f00ae
Chris Hartgerink <chris@libscie.org>
2021-10-13 10:40:35 +0200

    Update module query

--- app/modules/queries/useCurrentModule.ts
+++ app/modules/queries/useCurrentModule.ts
@@ -3,6 +3,13 @@ import db from "db"
 export default async function getCurrentWorkspace({ suffix }) {
   const module = await db.module.findFirst({
     where: { suffix },
+    include: {
+      authors: {
+        include: {
+          workspace: true,
+        },
+      },
+    },
   })
 
   return module
