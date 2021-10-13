18156b720b5cdc454c9bbd5c08920ae91e7aaea5
Chris Hartgerink <chris@libscie.org>
2021-10-13 13:11:06 +0200

    Revert publication approval after title/abstract changes

--- app/modules/mutations/changeAbstract.ts
+++ app/modules/mutations/changeAbstract.ts
@@ -2,9 +2,19 @@ import { NotFoundError, resolver } from "blitz"
 import db from "db"
 
 export default resolver.pipe(resolver.authorize(), async ({ suffix, description }) => {
-  await db.module.update({
+  const module = await db.module.update({
     where: { suffix },
     data: { description },
+  })
+
+  // Force all authors to reapprove for publishing
+  await db.authorship.updateMany({
+    where: {
+      moduleId: module.id,
+    },
+    data: {
+      readyToPublish: false,
+    },
   })
 
   return true
--- app/modules/mutations/changeTitle.ts
+++ app/modules/mutations/changeTitle.ts
@@ -2,9 +2,19 @@ import { NotFoundError, resolver } from "blitz"
 import db from "db"
 
 export default resolver.pipe(resolver.authorize(), async ({ suffix, title }) => {
-  await db.module.update({
+  const module = await db.module.update({
     where: { suffix },
-    data: { title }, // force email to be unverified upon change
+    data: { title },
+  })
+
+  // Force all authors to reapprove for publishing
+  await db.authorship.updateMany({
+    where: {
+      moduleId: module.id,
+    },
+    data: {
+      readyToPublish: false,
+    },
   })
 
   return true
