26965587512ebddce9e9bb663befc4231c74e6fc
Chris Hartgerink <chris@libscie.org>
2021-10-15 13:53:41 +0200

    Add note

--- app/modules/queries/useCurrentModule.ts
+++ app/modules/queries/useCurrentModule.ts
@@ -1,5 +1,6 @@
 import db from "db"
 
+// TODO: Sort by authorshipRank?
 export default async function getCurrentWorkspace({ suffix }) {
   const module = await db.module.findFirst({
     where: { suffix },
