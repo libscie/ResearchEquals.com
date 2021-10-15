46880cd8b6690013ca6ead382eaa4e207acdaf00
Chris Hartgerink <chris@libscie.org>
2021-10-15 13:52:16 +0200

    Update authorship mutations

--- app/authorship/mutations/approveAuthorship.ts
+++ app/authorship/mutations/approveAuthorship.ts
@@ -1,7 +1,7 @@
 import { resolver } from "blitz"
 import db from "db"
 
-export default resolver.pipe(resolver.authorize(), async ({ id }) => {
+export default resolver.pipe(resolver.authorize(), async ({ id, suffix }) => {
   await db.authorship.update({
     where: {
       id,
@@ -11,5 +11,18 @@ export default resolver.pipe(resolver.authorize(), async ({ id }) => {
     },
   })
 
-  return true
+  const module = await db.module.findFirst({
+    where: {
+      suffix,
+    },
+    include: {
+      authors: {
+        include: {
+          workspace: true,
+        },
+      },
+    },
+  })
+
+  return module!
 })
