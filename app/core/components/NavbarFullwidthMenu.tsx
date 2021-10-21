30944d0a3ceff53d5c55d86677dafd26e32e6821
Chris Hartgerink <chris@libscie.org>
2021-10-21 15:29:48 +0200

    Update dashboard to use query and refetch

--- app/core/components/NavbarFullwidthMenu.tsx
+++ app/core/components/NavbarFullwidthMenu.tsx
@@ -185,12 +185,11 @@ const FullWidthMenu = () => {
           </Transition>
         </Menu>
         {/* TODO: Add action */}
-        <a
-          href="#"
-          className="ml-6 inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-        >
-          Create module
-        </a>
+        <Link href={Routes.CreatePage()}>
+          <a className="ml-6 inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+            Create module
+          </a>
+        </Link>
       </div>
     )
   } else {
