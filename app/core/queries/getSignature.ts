0223deb5d717e54797e8885db61ba4e6d8d773fc
Chris Hartgerink <chris@libscie.org>
2021-10-28 14:05:13 +0200

    Remove old key

--- app/core/queries/getSignature.ts
+++ app/core/queries/getSignature.ts
@@ -2,7 +2,7 @@ import generateSignature from "app/signature"
 
 export default async function getSignature() {
   const expire = Math.round(Date.now() / 1000) + 120
-  const signature = generateSignature("c1dab0a41e2ce88da42c", expire.toString())
+  const signature = generateSignature(process.env.UPLOADCARE_SECRET_KEY, expire.toString())
 
   return { signature, expire }
 }
