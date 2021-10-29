89cc88d2244a4662cf9f958c8627a140fce4ea5f
Chris Hartgerink <chris@libscie.org>
2021-10-29 17:07:53 +0200

    Add follow unfollow mutations

---
+++ app/workspaces/mutations/followWorkspace.ts
@@ -0,0 +1,28 @@
+import { resolver } from "blitz"
+import db from "db"
+import { z } from "zod"
+
+const FollowWorkspace = z.object({
+  followerId: z.number(),
+  followedId: z.number(),
+})
+
+export default resolver.pipe(
+  resolver.zod(FollowWorkspace),
+  resolver.authorize(),
+  async ({ followerId, followedId, ...data }) => {
+    // TODO: in multi-tenant app, you must add validation to ensure correct tenant
+    const workspace = await db.workspace.update({
+      where: { id: followerId },
+      data: {
+        following: {
+          connect: {
+            id: followedId,
+          },
+        },
+      },
+    })
+
+    return workspace
+  }
+)
