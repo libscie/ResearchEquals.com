098d8808d624e2ceb4702f1a6df62ac6830d392f
Chris Hartgerink <chris@libscie.org>
2021-10-31 00:57:27 +0200

    Move getSignature to auth

--- app/core/modals/settings.tsx
+++ app/core/modals/settings.tsx
@@ -15,7 +15,7 @@ import { Form, FORM_ERROR } from "../components/Form"
 import { LabeledTextField } from "app/core/components/LabeledTextField"
 import { ChangePassword, ChangeEmail, ChangeName } from "app/auth/validations"
 import DeleteModal from "./delete"
-import getSignature from "../queries/getSignature"
+import getSignature from "../../auth/getSignature"
 
 function classNames(...classes) {
   return classes.filter(Boolean).join(" ")
