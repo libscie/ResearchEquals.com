549288dcf133f1e280d781c28076c3af874bbc10
Chris Hartgerink <chris@libscie.org>
2021-10-30 10:31:15 +0200

    Add rudimentary paginatable query

---
+++ app/workspaces/queries/getFeed.ts
@@ -0,0 +1,28 @@
+import { paginate, resolver } from "blitz"
+import db, { Prisma } from "db"
+
+interface GetModuleInput
+  extends Pick<Prisma.ModuleFindManyArgs, "where" | "orderBy" | "skip" | "take"> {}
+export default resolver.pipe(
+  resolver.authorize(),
+  async ({ where, orderBy, skip = 0, take = 100 }: GetModuleInput) => {
+    const {
+      items: modules,
+      hasMore,
+      nextPage,
+      count,
+    } = await paginate({
+      skip,
+      take,
+      count: () => db.module.count({ where }),
+      query: (paginateArgs) => db.module.findMany({ ...paginateArgs, where, orderBy }),
+    })
+
+    return {
+      modules,
+      nextPage,
+      hasMore,
+      count,
+    }
+  }
+)
