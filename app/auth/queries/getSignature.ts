ce584fb8338e86a93d4540e9d4a6280d72c378d0
Chris Hartgerink <chris@libscie.org>
2021-11-01 10:37:28 +0100

    Add logger

--- app/auth/queries/getSignature.ts
+++ app/auth/queries/getSignature.ts
@@ -2,6 +2,7 @@ import generateSignature from "app/signature"
 
 export default async function getSignature() {
   const expire = Math.round(Date.now() / 1000) + 120
+  console.log(process.env)
   const signature = generateSignature(process.env.UPLOADCARE_SECRET_KEY, expire.toString())
 
   return { signature, expire }
