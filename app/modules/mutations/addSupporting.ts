61274ecdb10b3310f0e88dadc9327fd6e2da4e3b
Chris Hartgerink <chris@libscie.org>
2021-11-11 15:12:56 +0100

    Add supporting mutation

---
+++ app/modules/mutations/addSupporting.ts
@@ -0,0 +1,24 @@
+import { NotFoundError, resolver } from "blitz"
+import db from "db"
+import { Prisma } from "prisma"
+
+export default resolver.pipe(resolver.authorize(), async ({ suffix, json }) => {
+  const module = await db.module.update({
+    where: { suffix },
+    data: { supporting: json as Prisma.JsonObject },
+  })
+
+  const updatedModule = await db.module.findFirst({
+    where: { suffix },
+    include: {
+      authors: {
+        include: {
+          workspace: true,
+        },
+      },
+      license: true,
+    },
+  })
+
+  return updatedModule!
+})
