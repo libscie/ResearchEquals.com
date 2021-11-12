29b44664647613515f6901791402afca1b1b987f
Chris Hartgerink <chris@libscie.org>
2021-11-12 10:01:49 +0100

    Add file display component

---
+++ app/core/components/EditFileDisplay.tsx
@@ -0,0 +1,21 @@
+import { Download32, TrashCan32 } from "@carbon/icons-react"
+
+const EditFileDisplay = ({ name, size, url }) => {
+  return (
+    <div className="flex my-2">
+      <p className="flex-grow flex border-2 border-gray-700 px-2 py-1 hover:bg-indigo-500">
+        <span className="flex-grow">{name}</span>
+        <span>{size / 1000}KB</span>
+      </p>
+      <p className="flex">
+        <a href={url} download>
+          <Download32 />
+        </a>
+        {/* TODO: Add action */}
+        <TrashCan32 />
+      </p>
+    </div>
+  )
+}
+
+export default EditFileDisplay
