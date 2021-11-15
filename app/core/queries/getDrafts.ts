8c6e3dd17006d3a164adc03f9940f2cee51e5cbd
Chris Hartgerink <chris@libscie.org>
2021-11-15 15:47:02 +0100

    Return authors in drafts

--- app/core/queries/getDrafts.ts
+++ app/core/queries/getDrafts.ts
@@ -16,6 +16,14 @@ export default async function getSignature({ session }) {
         updatedAt: "desc",
       },
     ],
+    include: {
+      type: true,
+      authors: {
+        include: {
+          workspace: true,
+        },
+      },
+    },
   })
 
   return draftModules
