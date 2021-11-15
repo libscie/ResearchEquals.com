901bb0b8cc3ced0698f1437f1607083ae0530258
Chris Hartgerink <chris@libscie.org>
2021-11-15 15:26:17 +0100

    Update author styling

--- app/modules/components/ManageAuthors.tsx
+++ app/modules/components/ManageAuthors.tsx
@@ -110,9 +110,9 @@ const ManageAuthors = ({ open, setOpen, moduleEdit, setQueryData }) => {
                     >
                       <div className="flex flex-col">
                         <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
-                          <div className="py-2 align-middle inline-block sm:px-6 lg:px-8">
+                          <div className="py-2 align-middle inline-block sm:px-6 lg:px-8 w-full">
                             <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
-                              <table className="divide-y divide-gray-200">
+                              <table className="divide-y divide-gray-200 w-full">
                                 <thead className="bg-gray-50">
                                   <tr>
                                     <th
