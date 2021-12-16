a32324f5cbcca9b5e21fe44e2bf0192d2a9647cc
Chris Hartgerink <chris@libscie.org>
2021-12-16 10:51:09 +0100

    Add db seed script

--- db/seeds.ts
+++ db/seeds.ts
@@ -1,4 +1,4 @@
-// import db from "./index"
+import db from "db"
 
 /*
  * This seed function is executed when you run `blitz db seed`.
@@ -8,9 +8,51 @@
  * realistic data.
  */
 const seed = async () => {
-  // for (let i = 0; i < 5; i++) {
-  //   await db.project.create({ data: { name: "Project " + i } })
-  // }
+  await db.license.createMany({
+    data: [
+      {
+        url: "https://creativecommons.org/publicdomain/zero/1.0/legalcode",
+        name: "CC0 Public Domain Dedication",
+        price: 0,
+      },
+      {
+        url: "https://creativecommons.org/licenses/by/4.0/legalcode",
+        name: "CC BY 4.0",
+        price: 0,
+      },
+      {
+        url: "https://creativecommons.org/licenses/by-nc-nd/4.0/legalcode",
+        name: "CC BY-NC-ND 4.0",
+        price: 42999,
+      },
+      {
+        url: "https://creativecommons.org/licenses/by-nc-sa/4.0/",
+        name: "CC BY-NC-SA 4.0",
+        price: 32999,
+      },
+      {
+        url: "https://creativecommons.org/licenses/by-nd/4.0/legalcode",
+        name: "CC BY-ND 4.0",
+        price: 24999,
+      },
+      {
+        url: "https://creativecommons.org/licenses/by-nc/4.0/legalcode",
+        name: "CC BY-NC 4.0",
+        price: 19499,
+      },
+      {
+        url: "https://creativecommons.org/licenses/by-sa/4.0/legalcode",
+        name: "CC BY-SA 4.0",
+        price: 14999,
+      },
+      {
+        url: "",
+        name: "All rights reserved",
+        price: 54999,
+      },
+    ],
+    skipDuplicates: true,
+  })
 }
 
 export default seed
