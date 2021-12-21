6733076a07b11d8611203d813f9929509052cc9a
Chris Hartgerink <chris@libscie.org>
2021-12-21 16:26:06 +0100

    Add parameter check

--- app/api/checkout_sessions.js
+++ app/api/checkout_sessions.js
@@ -1,34 +1,35 @@
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY)
 
 export default async function handler(req, res) {
-  if (req.method === "POST") {
-    try {
-      // Create Checkout Sessions from body params.
-      const session = await stripe.checkout.sessions.create({
-        customer_email: "customer@example.com",
-        submit_type: "donate",
-        billing_address_collection: "auto",
-        shipping_address_collection: {
-          allowed_countries: ["US", "CA"],
-        },
-        line_items: [
-          {
-            // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
-            price: "price_1K98axLmgtJbKHNGBo3BcDFC",
-            quantity: 1,
-          },
-        ],
-        mode: "payment",
-        success_url: `${req.headers.origin}/?success=true`,
-        cancel_url: `${req.headers.origin}/?canceled=true`,
-        automatic_tax: { enabled: true },
-      })
-      res.redirect(303, session.url)
-    } catch (err) {
-      res.status(err.statusCode || 500).json(err.message)
-    }
+  if (!req.query.email || !req.query.price_id || !req.query.suffix) {
+    res.status(500).end("Incomplete request")
   } else {
-    res.setHeader("Allow", "POST")
-    res.status(405).end("Method Not Allowed")
+    if (req.method === "POST") {
+      try {
+        // Create Checkout Sessions from body params.
+        const session = await stripe.checkout.sessions.create({
+          customer_email: "customer@example.com",
+          submit_type: "pay",
+          billing_address_collection: "auto",
+          line_items: [
+            {
+              // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
+              price: "price_1K98axLmgtJbKHNGBo3BcDFC",
+              quantity: 1,
+            },
+          ],
+          mode: "payment",
+          success_url: `${req.headers.origin}/?success=true`,
+          cancel_url: `${req.headers.origin}/?canceled=true`,
+          automatic_tax: { enabled: true },
+        })
+        res.redirect(303, session.url)
+      } catch (err) {
+        res.status(err.statusCode || 500).json(err.message)
+      }
+    } else {
+      res.setHeader("Allow", "POST")
+      res.status(405).end("Method Not Allowed")
+    }
   }
 }
