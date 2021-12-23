ba4f82e8b95ad8f28f65c5b5b4b1bd1d22c02cfc
Chris Hartgerink <chris@libscie.org>
2021-12-23 14:48:24 +0100

    Update module card styling

--- app/core/components/ModuleCard.tsx
+++ app/core/components/ModuleCard.tsx
@@ -1,29 +1,43 @@
 const ModuleCard = ({ type, title, status, time, authors }) => {
   return (
-    <div className="flex flex-col h-32 w-full bg-gray-300 hover:bg-gray-100 text-base text-black p-2">
+    <div className="flex flex-col h-32 w-full hover:bg-gray-50 dark:hover:bg-gray-800 px-4 py-2">
       <div className="flex-grow">
-        <p className="text-xs text-gray-500">{type}</p>
-        <h2 className="text-base line-clamp-2">{title}</h2>
+        <p className="text-xs leading-4 font-normal text-gray-500 dark:text-gray-400">{type}</p>
+        <h2 className="text-sm leading-5 font-medium text-gray-900 dark:text-gray-200 line-clamp-2">
+          {title}
+        </h2>
       </div>
       <div className="flex">
-        <div className="flex-grow text-xs text-gray-500">
+        <div className="flex-grow text-xs leading-4 font-normal text-gray-500 dark:text-gray-400">
           <p>{status}</p>
-          <p>{time}</p>
+          <p>Published: {time}</p>
         </div>
-        <div className="flex -space-x-1 relative z-0 overflow-hidden">
-          <div className="inline-block h-full align-middle">
-            <span className="inline-block h-full align-middle"> </span>
-
-            {authors.map((author) => (
-              <>
+        <div className="flex -space-x-1 relative z-0 overflow-hidden p-1">
+          {authors.map((author, index) => (
+            <>
+              {index < 4 ? (
                 <img
-                  className="relative z-30 inline-block h-6 w-6 rounded-full"
+                  className="relative inline-block h-6 w-6 rounded-full ring-1 ring-white dark:ring-gray-800"
                   src={author.workspace.avatar}
                   alt={`Avatar of ${author.workspace.handle}`}
+                  style={{ zIndex: 100 - index }}
                 />
-              </>
-            ))}
-          </div>
+              ) : (
+                ""
+              )}
+            </>
+          ))}
+          {/* </div> */}
+          {authors.length >= 4 ? (
+            <>
+              <span className="inline-block h-full align-middle"> </span>
+              <span className="inline-flex items-center px-3 py-0.5 rounded-full text-xs leading-4 font-medium text-gray-700 dark:text-gray-200 bg-white dark:bg-gray-700 ring-1 ring-gray-300 dark:ring-gray-600 max-h-6 shadow-sm dark:shadow-none">
+                + {authors.length}
+              </span>
+            </>
+          ) : (
+            ""
+          )}
         </div>
       </div>
     </div>
