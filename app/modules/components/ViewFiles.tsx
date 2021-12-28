e51c837612774c2ad73acff306173900e2eebbd9
Chris Hartgerink <chris@libscie.org>
2021-12-28 19:48:40 +0100

    Upgrade file display

--- app/modules/components/ViewFiles.tsx
+++ app/modules/components/ViewFiles.tsx
@@ -1,15 +1,23 @@
-import { Download32 } from "@carbon/icons-react"
+import { Download24 } from "@carbon/icons-react"
+import filesize from "filesize"
 
 const ViewFiles = ({ name, size, url }) => {
   return (
     <div className="flex my-2">
-      <p className="flex-grow flex border-2 border-gray-700 px-2 py-1 hover:bg-indigo-500">
-        <span className="flex-grow">{name}</span>
-        <span>{size / 1000}KB</span>
-      </p>
+      <a
+        href={url}
+        className="flex-grow flex border rounded border-gray-100 dark:border-gray-600 shadow-sm px-2 py-1 hover:bg-gray-100 dark:hover:bg-gray-700 text-xs leading-4 font-normal text-gray-900 dark:text-gray-200 mr-1"
+        target="_blank"
+        download
+        rel="noreferrer"
+      >
+        <span className="flex-grow  inline-block align-middle">{name}</span>
+        {/* https://www.npmjs.com/package/filesize */}
+        <span>{filesize(size)}</span>
+      </a>
       <p className="flex">
         <a href={url} target="_blank" download rel="noreferrer">
-          <Download32 />
+          <Download24 className="w-6 h-6 fill-current text-gray-900 dark:text-gray-200 inline-block align-middle" />
         </a>
       </p>
     </div>
