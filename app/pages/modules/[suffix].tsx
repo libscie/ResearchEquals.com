c58a484f36367ffa68ea0a36949846d199795f75
Chris Hartgerink <chris@libscie.org>
2021-12-29 11:40:27 +0100

    Add metadata display to published modules

--- app/pages/modules/[suffix].tsx
+++ app/pages/modules/[suffix].tsx
@@ -1,17 +1,11 @@
-import moment from "moment"
-import { Share32, DocumentPdf32, AddAlt32 } from "@carbon/icons-react"
 import { Prisma } from "prisma"
 import { NotFoundError } from "blitz"
 
 import Layout from "../../core/layouts/Layout"
 import db from "db"
 import NavbarApp from "../../core/components/Navbar"
-import MetadataView from "../../modules/components/MetadataView"
 import ViewFiles from "../../modules/components/ViewFiles"
-import ViewAuthors from "../../modules/components/ViewAuthors"
-import FollowsFromView from "../../modules/components/ParentView"
-import LeadsToView from "../../modules/components/ChildView"
-import AuthorAvatars from "../../modules/components/AuthorAvatars"
+import MetadataImmutable from "../../modules/components/MetadataImmutable"
 
 export async function getServerSideProps(context) {
   const module = await db.module.findFirst({
@@ -65,59 +59,29 @@ const ModulePage = ({ module }) => {
   return (
     <Layout title={`R=${module.title}`}>
       <NavbarApp />
-      <div className="max-w-7xl sm:mx-auto my-4 mx-2">
-        <div className="w-full bg-gray-300 flex">
+      <main className="max-w-7xl sm:mx-auto my-4 mx-4">
+        <div className="w-full flex">
+          {/* Push all menu bars to the right */}
           <div className="flex-grow"></div>
-          <Share32 />
-          <DocumentPdf32 />
-          <AddAlt32 />
-        </div>
-        <div className="text-center">
-          Published {moment(module.publishedAt).fromNow()} ({module.publishedAt.toUTCString()})
-        </div>
-        <div className="w-full flex my-8">
-          <FollowsFromView module={module} />
-          <svg
-            className="fill-current text-black w-8 h-8 mx-4 "
-            viewBox="0 0 32 32"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              fillRule="evenodd"
-              clipRule="evenodd"
-              d="M27 13L32 15.8868L27 18.7735V16.3868H26C21.5298 16.3868 17.7807 19.4742 16.7689 23.6331C16.6473 23.0488 16.4793 22.4815 16.2689 21.9352C17.83 18.0946 21.5988 15.3868 26 15.3868H27V13Z"
-            />
-            <path
-              fillRule="evenodd"
-              clipRule="evenodd"
-              d="M6 16.3867H0V15.3867H6C11.799 15.3867 16.5 20.0877 16.5 25.8867V26.8867H18.8868L16 31.8867L13.1132 26.8867H15.5V25.8867C15.5 20.64 11.2467 16.3867 6 16.3867Z"
-            />
-          </svg>
-          <LeadsToView module={module} />
-        </div>
-        {/* metadata */}
-        {/* <MetadataView
-          module={module}
-          addAuthors={manageAuthorsOpen}
-          setQueryData={setQueryData}
-          setAddAuthors={setManageAuthorsOpen}
-        /> */}
-
-        {/* main file */}
-        <div className="flex border-t border-b border-gray-800 mt-2 py-2">
-          <div className="flex-grow flex -space-x-2 relative z-0 overflow-hidden">
-            <AuthorAvatars module={module} />
-          </div>
-          <ViewAuthors button="button" module={module} />
-        </div>
-        <div>
           <div>
-            <h2>Main file</h2>
+            {/* TODO: Add actions */}
+            {/* <AddAlt24 className="h-6 w-6 fill-current text-gray-300 dark:text-gray-600" /> */}
+          </div>
+        </div>
+        <MetadataImmutable module={module} />
+
+        {mainFile.name ? (
+          <div className="my-8">
+            <h2 className="">Main file</h2>
             <ViewFiles name={mainFile.name} size={mainFile.size} url={mainFile.cdnUrl} />
           </div>
-          <div className="pb-16">
-            <h2>Supporting files</h2>
+        ) : (
+          ""
+        )}
+        {/* Supporting files */}
+        {supportingRaw.files.length > 0 ? (
+          <div className="my-8">
+            <h2>Supporting file(s)</h2>
             {supportingRaw.files.map((file) => (
               <>
                 <ViewFiles
@@ -128,8 +92,11 @@ const ModulePage = ({ module }) => {
               </>
             ))}
           </div>
-        </div>
-      </div>
+        ) : (
+          ""
+        )}
+        {/* PLACEHOLDER References */}
+      </main>
     </Layout>
   )
 }
