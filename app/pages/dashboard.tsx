5bc1d00f724dafa88be8de09aa472906ad6a9f8e
Chris Hartgerink <chris@libscie.org>
2021-12-29 12:55:56 +0100

    Add Feed display to dashboard, upgrade WhoToFollow

--- app/pages/dashboard.tsx
+++ app/pages/dashboard.tsx
@@ -23,6 +23,8 @@ import followWorkspace from "../workspaces/mutations/followWorkspace"
 import unfollowWorkspace from "../workspaces/mutations/unfollowWorkspace"
 import getFeed from "../workspaces/queries/getFeed"
 import ModuleCard from "../core/components/ModuleCard"
+import FollowButton from "app/workspaces/components/FollowButton"
+import FeedPagination from "../core/components/FeedPagination"
 
 const ITEMS_PER_PAGE = 10
 
@@ -66,16 +68,15 @@ const DashboardContent = () => {
           <Banner message="You can only start publishing once your email is verified. Please check your inbox." />
         )}
 
-        <div className="lg:flex flex-row">
+        <div className="lg:flex flex-row text-gray-900 dark:text-gray-200">
           {/* Column 1 */}
           <div className="lg:w-1/4 p-4">
             <div className="my-2">
-              <h1 className="text-4xl font-medium text-gray-900">
+              <h1 className="text-4xl font-medium">
                 Welcome back,{" "}
                 {data.workspace!.name ? data.workspace!.name : "@" + data.workspace!.handle}!
               </h1>
             </div>
-            <p className="text-gray-900">Get to work here</p>
             <dl className="mt-2 bg-gray-100 hover:bg-gray-50 overflow-hidden shadow divide-y divide-gray-200 md:grid-cols-3 md:divide-y-0 md:divide-x">
               {stats.map((item) => (
                 <div key={item.name} className="px-4 py-5 sm:p-6">
@@ -101,82 +102,45 @@ const DashboardContent = () => {
             <div className="sm:flex w-full mt-4">
               <OnboardingQuests data={data} />
             </div>
-            <div className="my-4">
-              <h2 className="font-bold text-4xl">Feed</h2>
+            <div className="my-2">
               {modules.length > 0 ? (
-                <div>
-                  <ul role="list" className="divide-y divide-gray-200 my-2">
-                    {modules.map((module) => (
-                      <>
-                        <li
-                          onClick={() => {
-                            router.push(`/modules/${module.suffix}`)
-                          }}
-                          className="cursor-pointer"
-                        >
-                          <ModuleCard
-                            type={module.type}
-                            title={module.title}
-                            status={`DOI: 10.53962/${module.suffix}`}
-                            time={moment(module.publishedAt).fromNow()}
-                            timeText="Published"
-                            authors={module.authors}
-                          />
-                        </li>
-                      </>
-                    ))}
-                  </ul>
-                  {/* TODO: Put into one component - also used in [handle].tsx */}
-                  <div className="flex my-1">
-                    <div className="flex-1 flex items-center justify-between">
-                      <p className="text-sm text-gray-700">
-                        Showing <span className="font-medium">{ITEMS_PER_PAGE * page + 1}</span> to{" "}
-                        <span className="font-medium">
-                          {ITEMS_PER_PAGE + page > count ? count : ITEMS_PER_PAGE + page}
-                        </span>{" "}
-                        of <span className="font-medium">{count}</span> results
-                      </p>
-                    </div>
-                    <nav
-                      className="relative z-0 inline-flex rounded-md -space-x-px"
-                      aria-label="Pagination"
-                    >
-                      <button
-                        className="relative inline-flex items-center px-2 py-2 bg-gray-300 text-sm font-medium text-gray-500 hover:bg-gray-400"
-                        disabled={page === 0}
-                        onClick={goToPreviousPage}
-                      >
-                        <span className="sr-only">Previous</span>
-                        <ChevronLeftIcon className="h-5 w-5" aria-hidden="true" />
-                      </button>
-                      {Array.from({ length: Math.ceil(count / ITEMS_PER_PAGE) }, (x, i) => i).map(
-                        (pageNr) => (
-                          <button
-                            key={`page-nav-feed-${pageNr}`}
-                            className="relative inline-flex items-center px-2 py-2 bg-gray-300 text-sm font-medium text-gray-500 hover:bg-gray-400"
-                            disabled={page === pageNr}
+                <>
+                  <div className="rounded-t-md border border-gray-300 dark:border-gray-600 mt-8 divide-y divide-gray-300 dark:divide-gray-600">
+                    <h1 className="text-xs leading-4 font-medium mx-4 my-2 text-gray-500 dark:text-gray-400 ">
+                      Published modules
+                    </h1>
+                    <ul role="list" className="divide-y divide-gray-300 dark:divide-gray-600">
+                      {modules.map((module) => (
+                        <>
+                          <li
                             onClick={() => {
-                              goToPage(pageNr)
+                              router.push(`/modules/${module.suffix}`)
                             }}
+                            className="cursor-pointer"
                           >
-                            <span className="sr-only">Navigate to page {pageNr}</span>
-                            <span className="h-5 w-5 " aria-hidden="true">
-                              {pageNr + 1}
-                            </span>
-                          </button>
-                        )
-                      )}
-                      <button
-                        className="relative inline-flex items-center px-2 py-2 bg-gray-300 text-sm font-medium text-gray-500 hover:bg-gray-400"
-                        disabled={!hasMore}
-                        onClick={goToNextPage}
-                      >
-                        <span className="sr-only">Previous</span>
-                        <ChevronRightIcon className="h-5 w-5" aria-hidden="true" />
-                      </button>
-                    </nav>
+                            <ModuleCard
+                              type={module.type.name}
+                              title={module.title}
+                              status={`DOI: 10.53962/${module.suffix}`}
+                              time={moment(module.publishedAt).fromNow()}
+                              timeText="Published"
+                              authors={module.authors}
+                            />
+                          </li>
+                        </>
+                      ))}
+                    </ul>
                   </div>
-                </div>
+                  <FeedPagination
+                    ITEMS_PER_PAGE={ITEMS_PER_PAGE}
+                    page={page}
+                    count={count}
+                    goToPreviousPage={goToPreviousPage}
+                    goToPage={goToPage}
+                    goToNextPage={goToNextPage}
+                    hasMore={hasMore}
+                  />
+                </>
               ) : (
                 <div className="flex flex-col flex-grow relative w-full border-2 border-gray-1000 border-dashed rounded-lg text-center focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500  my-4 h-auto">
                   <div className="table flex-grow w-full">
@@ -233,7 +197,7 @@ const Dashboard = () => {
   return (
     <>
       <Navbar />
-      <main className="max-w-7xl lg:max-w-full mx-auto bg-gray-300 max-h-full h-full">
+      <main className="max-w-7xl lg:max-w-full mx-auto max-h-full h-full">
         <Suspense fallback="Loading...">
           <DashboardContent />
         </Suspense>
@@ -243,69 +207,36 @@ const Dashboard = () => {
 }
 
 const WhoToFollow = ({ data, refetch, refetchFeed }) => {
-  const [followWorkspaceMutation] = useMutation(followWorkspace)
-  const [unfollowWorkspaceMutation] = useMutation(unfollowWorkspace)
+  const refetchFn = () => {
+    refetch()
+    refetchFeed()
+  }
 
   return (
     <>
-      <h2 className="font-bold text-4xl my-2 text-black">Who to follow</h2>
-      {data.followableWorkspaces.map((workspace) => (
-        <div
-          key={workspace.id + workspace.handle}
-          className="flex bg-gray-100 p-2 hover:bg-gray-50"
-        >
-          <Link href={Routes.HandlePage({ handle: workspace.handle })}>
-            <a className="flex-grow flex">
-              <img className="w-10 h-10 rounded-full mr-2" src={workspace!.avatar!} />
-              <p className="flex-grow">
-                <span className="inline-block h-full align-middle"> </span>
-                <p className="inline-block align-middle truncate">
-                  {workspace.name ? (
-                    <>
-                      <span>{workspace.name}</span> -{" "}
-                      <span className="text-gray-500">@{workspace.handle}</span>
-                    </>
-                  ) : (
-                    <span className="text-black">@{workspace.handle}</span>
-                  )}
+      <h2 className="text-2xl font-medium my-2">Who to follow</h2>
+      {data.followableWorkspaces.map((author) => (
+        <>
+          <li className="py-2 flex">
+            <div className="mr-2">
+              <img
+                src={author.avatar}
+                alt={`Avatar of ${author.name}`}
+                className="w-10 h-10 rounded-full inline-block h-full align-middle"
+              />
+            </div>
+            <div className="flex-grow">
+              <span className="inline-block h-full align-middle"></span>
+              <p className="text-gray-700 dark:text-gray-200 text-sm leading-4 font-normal my-auto inline-block align-middle">
+                {author.name}
+                <p className="text-gray-500 dark:text-gray-400 text-xs leading-4 font-normal">
+                  @{author.handle}
                 </p>
               </p>
-            </a>
-          </Link>
-          <button
-            type="button"
-            className="inline-flex items-center px-2.5 py-1.5 border border-transparent text-xs font-medium rounded shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-            onClick={async () => {
-              await followWorkspaceMutation({
-                followedId: workspace.id,
-              })
-
-              toast((t) => (
-                <span>
-                  Custom and <b>bold</b>
-                  <button
-                    onClick={async () => {
-                      await unfollowWorkspaceMutation({
-                        followedId: workspace.id,
-                      })
-                      refetch()
-                      refetchFeed()
-                      toast.dismiss(t.id)
-                    }}
-                  >
-                    Undo
-                  </button>
-                </span>
-              ))
-              refetch()
-              refetchFeed()
-            }}
-          >
-            {data.workspace!.following.filter((x) => x.id === workspace.id).length > 0
-              ? "Following"
-              : "Follow"}
-          </button>
-        </div>
+            </div>
+            <FollowButton author={author} refetchFn={refetchFn} />
+          </li>
+        </>
       ))}
     </>
   )
