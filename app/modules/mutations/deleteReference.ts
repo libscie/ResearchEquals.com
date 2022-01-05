a9ba7fe555be4ca135cc3669faab71c5288f216f
Chris Hartgerink <chris@libscie.org>
2022-01-05 10:04:12 +0100

    Add deleteReference

---
+++ app/modules/mutations/deleteReference.ts
@@ -0,0 +1,49 @@
+import { resolver } from "blitz"
+import db from "db"
+
+export default resolver.pipe(resolver.authorize(), async ({ currentId, disconnectId }) => {
+  const module = await db.module.update({
+    where: { id: currentId },
+    data: {
+      references: {
+        disconnect: { id: parseInt(disconnectId) },
+      },
+    },
+    include: {
+      references: {
+        orderBy: {
+          title: "asc",
+        },
+      },
+      authors: {
+        include: {
+          workspace: true,
+        },
+      },
+      license: true,
+      type: true,
+      parents: {
+        include: {
+          type: true,
+          authors: {
+            include: {
+              workspace: true,
+            },
+          },
+        },
+      },
+      children: {
+        include: {
+          type: true,
+          authors: {
+            include: {
+              workspace: true,
+            },
+          },
+        },
+      },
+    },
+  })
+
+  return module
+})
