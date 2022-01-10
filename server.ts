2580700578d98de218d0ec4b70efeedcf41fa361
Chris Hartgerink <chris@libscie.org>
2022-01-10 09:54:08 +0100

    Try differently

--- server.ts
+++ server.ts
@@ -7,35 +7,23 @@ import { parse } from "url"
 
 const { PORT = "3000" } = process.env
 const dev = process.env.NODE_ENV !== "production"
-const app = blitz({ dev })
+const app = blitz({})
 const handle = app.getRequestHandler()
 
 app.prepare().then(() => {
-  https
-    .createServer((req, res) => {
-      // Be sure to pass `true` as the second argument to `url.parse`.
-      // This tells it to parse the query portion of the URL.
-      const parsedUrl = parse(req.url!, true)
-
-      handle(req, res, parsedUrl)
-    })
-    .listen(PORT, () => {
-      console.log(`Ready on ${process.env.APP_ORIGIN}`)
-    })
-
   http
     .createServer((req, res) => {
       // Be sure to pass `true` as the second argument to `url.parse`.
       // This tells it to parse the query portion of the URL.
       const parsedUrl = parse(req.url!, true)
 
-      if (process.env.ALGOLIA_PREFIX !== "local") {
+      if (!dev) {
         res.writeHead(301, { Location: "https://" + req.headers["host"] + req.url })
       }
 
       handle(req, res, parsedUrl)
     })
-    .listen("80", () => {
+    .listen(PORT, () => {
       console.log(`Ready on ${process.env.APP_ORIGIN}`)
     })
 })
