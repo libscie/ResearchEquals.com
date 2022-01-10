47f8fa926cb82491a063b54b24e5f1e53fbf91ee
Chris Hartgerink <chris@libscie.org>
2022-01-10 09:24:23 +0100

    Add double server

--- server.ts
+++ server.ts
@@ -1,6 +1,7 @@
 // server.ts
 import blitz from "blitz/custom-server"
-import { createServer } from "http"
+import http from "http"
+import https from "https"
 
 import { parse } from "url"
 
@@ -10,17 +11,31 @@ const app = blitz({ dev })
 const handle = app.getRequestHandler()
 
 app.prepare().then(() => {
-  createServer((req, res) => {
-    // Be sure to pass `true` as the second argument to `url.parse`.
-    // This tells it to parse the query portion of the URL.
-    const parsedUrl = parse(req.url!, true)
+  https
+    .createServer((req, res) => {
+      // Be sure to pass `true` as the second argument to `url.parse`.
+      // This tells it to parse the query portion of the URL.
+      const parsedUrl = parse(req.url!, true)
 
-    if (process.env.ALGOLIA_PREFIX !== "local") {
-      res.writeHead(301, { Location: "https://" + req.headers["host"] + req.url })
-    }
+      handle(req, res, parsedUrl)
+    })
+    .listen(PORT, () => {
+      console.log(`Ready on ${process.env.APP_ORIGIN}`)
+    })
 
-    handle(req, res, parsedUrl)
-  }).listen(PORT, () => {
-    console.log(`Ready on ${process.env.APP_ORIGIN}`)
-  })
+  http
+    .createServer((req, res) => {
+      // Be sure to pass `true` as the second argument to `url.parse`.
+      // This tells it to parse the query portion of the URL.
+      const parsedUrl = parse(req.url!, true)
+
+      if (process.env.ALGOLIA_PREFIX !== "local") {
+        res.writeHead(301, { Location: "https://" + req.headers["host"] + req.url })
+      }
+
+      handle(req, res, parsedUrl)
+    })
+    .listen("80", () => {
+      console.log(`Ready on ${process.env.APP_ORIGIN}`)
+    })
 })
