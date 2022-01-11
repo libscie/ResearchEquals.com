ac869305d279590b80a4d36c95955b5fa1100bfb
Chris Hartgerink <chris@libscie.org>
2022-01-11 11:11:03 +0100

    Update icons

--- app/auth/pages/signup.tsx
+++ app/auth/pages/signup.tsx
@@ -4,6 +4,8 @@ import { useFormik } from "formik"
 import { z } from "zod"
 import { useState } from "react"
 import { Switch } from "@headlessui/react"
+import { Close32, Checkmark32 } from "@carbon/icons-react"
+
 import signup from "../mutations/signup"
 import ResearchEqualsLogo from "../../core/components/ResearchEqualsLogo"
 
@@ -145,15 +147,7 @@ const SignupPage: BlitzPage = () => {
                     )}
                     aria-hidden="true"
                   >
-                    <svg className="h-3 w-3 text-gray-400" fill="none" viewBox="0 0 12 12">
-                      <path
-                        d="M4 8l2-2m0 0l2-2M6 6L4 4m2 2l2 2"
-                        stroke="currentColor"
-                        strokeWidth={2}
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                      />
-                    </svg>
+                    <Close32 className="h-3 w-3 text-gray-400 stroke-2 stroke-current" />
                   </span>
                   <span
                     className={classNames(
@@ -164,13 +158,7 @@ const SignupPage: BlitzPage = () => {
                     )}
                     aria-hidden="true"
                   >
-                    <svg
-                      className="h-3 w-3 text-indigo-600"
-                      fill="currentColor"
-                      viewBox="0 0 12 12"
-                    >
-                      <path d="M3.707 5.293a1 1 0 00-1.414 1.414l1.414-1.414zM5 8l-.707.707a1 1 0 001.414 0L5 8zm4.707-3.293a1 1 0 00-1.414-1.414l1.414 1.414zm-7.414 2l2 2 1.414-1.414-2-2-1.414 1.414zm3.414 2l4-4-1.414-1.414-4 4 1.414 1.414z" />
-                    </svg>
+                    <Checkmark32 className="h-3 w-3 text-indigo-600 stroke-2 stroke-current" />
                   </span>
                 </span>
               </Switch>
@@ -208,15 +196,7 @@ const SignupPage: BlitzPage = () => {
                     )}
                     aria-hidden="true"
                   >
-                    <svg className="h-3 w-3 text-gray-400" fill="none" viewBox="0 0 12 12">
-                      <path
-                        d="M4 8l2-2m0 0l2-2M6 6L4 4m2 2l2 2"
-                        stroke="currentColor"
-                        strokeWidth={2}
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                      />
-                    </svg>
+                    <Close32 className="h-3 w-3 text-gray-400 stroke-2 stroke-current" />
                   </span>
                   <span
                     className={classNames(
@@ -227,13 +207,7 @@ const SignupPage: BlitzPage = () => {
                     )}
                     aria-hidden="true"
                   >
-                    <svg
-                      className="h-3 w-3 text-indigo-600"
-                      fill="currentColor"
-                      viewBox="0 0 12 12"
-                    >
-                      <path d="M3.707 5.293a1 1 0 00-1.414 1.414l1.414-1.414zM5 8l-.707.707a1 1 0 001.414 0L5 8zm4.707-3.293a1 1 0 00-1.414-1.414l1.414 1.414zm-7.414 2l2 2 1.414-1.414-2-2-1.414 1.414zm3.414 2l4-4-1.414-1.414-4 4 1.414 1.414z" />
-                    </svg>
+                    <Checkmark32 className="h-3 w-3 text-indigo-600 stroke-2 stroke-current" />
                   </span>
                 </span>
               </Switch>
