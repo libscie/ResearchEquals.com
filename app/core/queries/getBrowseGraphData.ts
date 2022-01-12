e105134c12f2d0225269947f19441601bfca9f30
Chris Hartgerink <chris@libscie.org>
2022-01-12 09:21:17 +0100

    Add getBrowseGraphData

---
+++ app/core/queries/getBrowseGraphData.ts
@@ -0,0 +1,45 @@
+import db from "db"
+
+const itemCounter = (array, item) =>
+  array.flat(Infinity).filter((currentItem) => currentItem == item).length
+
+function onlyUnique(value, index, self) {
+  return self.indexOf(value) === index
+}
+
+export default async function getSignature() {
+  const modules = await db.module.findMany({
+    where: {
+      published: true,
+    },
+    orderBy: [
+      {
+        publishedAt: "asc",
+      },
+    ],
+    select: {
+      publishedAt: true,
+    },
+  })
+
+  const dates = modules.map((module) => {
+    return module.publishedAt?.toISOString().substr(0, 10)
+  })
+
+  let data = [] as any
+  const uniqDates = dates.filter(onlyUnique)
+  uniqDates.forEach((date, index) => {
+    const epochTime = new Date(date!)
+    data.push({
+      time: epochTime.getTime(),
+      modules:
+        index !== 0 ? itemCounter(dates, date) + data[index - 1].modules : itemCounter(dates, date),
+    })
+    // console.log(data[index - 1].workspaces)
+    // console.log(itemCounter(dates, date))
+  })
+
+  console.log(data)
+
+  return data
+}
