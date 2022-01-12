030f46bb5fd2a95d6198af8f2ffb12286744e0da
Chris Hartgerink <chris@libscie.org>
2022-01-12 09:48:56 +0100

    Rename TermsContainer to MdxContainer'

---
+++ app/core/components/MdxContainer.tsx
@@ -0,0 +1,9 @@
+const MdxContainer = ({ children }) => {
+  return (
+    <main className="mdx max-w-7xl mx-4 lg:mx-auto my-4 text-gray-900 dark:text-gray-200">
+      {children}
+    </main>
+  )
+}
+
+export default MdxContainer
--- app/core/components/TermsContainer.tsx
+++
@@ -1,5 +0,0 @@
-const TermsContainer = ({ children }) => {
-  return <main className="max-w-4xl mx-auto my-2">{children}</main>
-}
-
-export default TermsContainer
