0408c76570aaa4b2598b48c1972ed980d612a965
Chris Hartgerink <chris@libscie.org>
2022-01-13 19:21:03 +0100

    Restructure /dashboard to 1 column layout, improve statistics

--- app/pages/dashboard.tsx
+++ app/pages/dashboard.tsx
@@ -3,6 +3,7 @@ import Layout from "app/core/layouts/Layout"
 import React, { useEffect } from "react"
 import toast from "react-hot-toast"
 import moment from "moment"
+import { ArrowUp32, ArrowDown32 } from "@carbon/icons-react"
 
 import getDashboardData from "../core/queries/getDashboardData"
 import Navbar from "../core/components/Navbar"
@@ -17,6 +18,10 @@ import getCurrentWorkspace from "app/workspaces/queries/getCurrentWorkspace"
 import { useCurrentUser } from "app/core/hooks/useCurrentUser"
 
 const ITEMS_PER_PAGE = 5
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(" ")
+}
 
 export async function getServerSideProps(context) {
   // Expires in 30 minutes
@@ -55,10 +60,21 @@ const DashboardContent = ({
     {
       name: "Drafts",
       stat: data.draftModules.length,
+      change: data.draftPercentage,
     },
     {
       name: "Invitations",
       stat: data.invitedModules.length,
+      change: data.invitedPercentage,
+    },
+    {
+      name: "Modules",
+      stat: data.myPublishedModules.length,
+      change: data.modulesPercentage,
+    },
+    {
+      name: "Followers",
+      stat: data.workspace.followers.length,
     },
   ]
 
@@ -71,11 +87,20 @@ const DashboardContent = ({
   if (data) {
     return (
       <>
-        <div className="lg:flex flex-row text-gray-900 dark:text-gray-200">
+        <div className="text-gray-900 dark:text-gray-200">
           {/* Column 1 */}
-          <div className="lg:w-1/4 p-4">
+          <div className="p-4">
             <div className="my-0">
-              <h1 className="text-4xl font-medium">Welcome back, @{data.workspace!.handle}!</h1>
+              <h1 className="text-4xl font-medium text-center">
+                Welcome back,{" "}
+                {data.workspace.firstName && data.workspace.lastName
+                  ? `${data.workspace.firstName} ${data.workspace.lastName}`
+                  : `@${data.workspace!.handle}`}
+                !
+              </h1>
+            </div>
+            <div className="lg:flex w-full mt-4 gap-2">
+              <OnboardingQuests data={data} expire={expire} signature={signature} />
             </div>
             <dl className="mt-2 flex text-gray-900 dark:text-gray-200  overflow-hidden shadow dark:border rounded border-gray-100 dark:border-gray-600 divide-gray-100 dark:divide-gray-600 md:grid-cols-3 divide-x">
               {stats.map((item) => (
@@ -89,6 +114,36 @@ const DashboardContent = ({
                       <dd className="mt-1 flex justify-between items-baseline md:block lg:flex">
                         <div className="flex items-baseline text-2xl font-semibold text-indigo-600 dark:text-indigo-200">
                           {item.stat}
+                          {item.change ? (
+                            <p
+                              className={classNames(
+                                item.change === Infinity
+                                  ? "hidden"
+                                  : item.change > 0
+                                  ? "text-green-700 dark:text-green-500"
+                                  : "text-red-700 dark:text-red-500",
+                                "ml-2 flex items-baseline text-sm font-semibold"
+                              )}
+                            >
+                              {item.change > 0 ? (
+                                <ArrowUp32
+                                  className="self-center flex-shrink-0 h-5 w-5 text-green-500"
+                                  aria-hidden="true"
+                                />
+                              ) : (
+                                <ArrowDown32
+                                  className="self-center flex-shrink-0 h-5 w-5 text-red-500"
+                                  aria-hidden="true"
+                                />
+                              )}
+                              <span className="sr-only">
+                                {item.change > 0 ? "Increased" : "Decreased"} by
+                              </span>
+                              {item.change}%
+                            </p>
+                          ) : (
+                            ""
+                          )}
                         </div>
                       </dd>
                     </button>
@@ -96,24 +151,9 @@ const DashboardContent = ({
                 </>
               ))}
             </dl>
-            {data.followableWorkspaces.length > 0 ? (
-              <div className="hidden lg:inline">
-                <WhoToFollow
-                  data={data}
-                  workspace={ownWorkspace}
-                  refetch={refetch}
-                  refetchFeed={refetchWorkspace}
-                />
-              </div>
-            ) : (
-              ""
-            )}
           </div>
           {/* Column 2 */}
           <div className="flex w-full flex-col px-4">
-            <div className="lg:flex w-full mt-4">
-              <OnboardingQuests data={data} expire={expire} signature={signature} />
-            </div>
             <div className="my-2">
               {modules.length > 0 ? (
                 <>
@@ -172,7 +212,7 @@ const DashboardContent = ({
               )}
             </div>
             {data.followableWorkspaces.length > 0 ? (
-              <div className="inline lg:hidden mb-16">
+              <div className="mb-16">
                 <WhoToFollow
                   data={data}
                   workspace={ownWorkspace}
@@ -198,7 +238,8 @@ const Dashboard = ({ expire, signature }) => {
   const query = useRouterQuery()
   const [ownWorkspace, { refetch: refetchWorkspace }] = useQuery(getCurrentWorkspace, null)
   const router = useRouter()
-  const [data, { refetch }] = useQuery(getDashboardData, { session })
+  // TODO: Add user select
+  const [data, { refetch }] = useQuery(getDashboardData, { session, changeDays: 100 })
 
   return (
     <>
@@ -211,7 +252,7 @@ const Dashboard = ({ expire, signature }) => {
         invitations={data.invitedModules}
         refetchFn={refetch}
       />
-      <main className="max-w-7xl lg:max-w-full mx-auto max-h-full h-full">
+      <main className="max-w-7xl mx-auto">
         <DashboardContent
           expire={expire}
           signature={signature}
