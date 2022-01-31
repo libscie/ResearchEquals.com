0809feab01bf8009233957da4c35bdbc5176c4f5
Chris Hartgerink <chris@libscie.org>
2022-01-31 14:51:20 +0100

    Improve empty state display

--- app/pages/drafts.tsx
+++ app/pages/drafts.tsx
@@ -49,92 +49,94 @@ const DraftsContents = ({ expire, signature, currentWorkspace, session, user }) 
   }, [])
 
   return (
-    <div
-      className="w-screen flex divide-x divide-gray-100 dark:divide-gray-600"
-      style={{
-        height: biggerWindow ? "calc(100vh - 73px - 55px)" : "100%",
-      }}
-    >
+    <>
       <div
-        className={`${
-          !inboxOpen ? "hidden" : "inline"
-        } float-left w-full lg:w-80 divide-y-0 divide-gray-100 dark:divide-gray-600 overflow-y-auto`}
-        // style={{ minHeight: "calc(100vh - 74px - 54px)" }}
+        className="w-screen flex divide-x divide-gray-100 dark:divide-gray-600"
+        style={{
+          height: biggerWindow ? "calc(100vh - 73px - 55px)" : "100%",
+        }}
       >
-        <h1 className="lg:hidden text-lg leading-7 font-medium text-gray-900 dark:text-gray-200 px-4 sm:px-6 lg:px-8 py-4 border-b lg:border-b-0 border-gray-100 dark:border-gray-600">
-          Drafts
-        </h1>
-        <ul role="list" className="divide-y divide-gray-100 dark:divide-gray-600">
-          {drafts.map((draft) => (
-            <>
-              <li
-                onClick={() => {
-                  setModule(draft)
-                  setInboxOpen(biggerWindow)
-                  Router.push("/drafts", { query: { suffix: draft.suffix } })
-                }}
-                className="cursor-pointer"
-              >
-                <ModuleCard
-                  type={draft.type.name}
-                  title={draft.title}
-                  status="Draft"
-                  time={moment(draft.updatedAt).fromNow()}
-                  timeText="Updated"
-                  authors={draft.authors}
-                />
-              </li>
-            </>
-          ))}
-        </ul>
-      </div>
-      {drafts.length > 0 ? (
-        <>
-          <div
-            className={`${
-              inboxOpen ? "hidden lg:inline" : "inline"
-            } flex-grow w-2/3 float-right overflow-y-auto`}
-          >
-            {currentModule ? (
-              <Suspense
-                fallback={
-                  <div className="mx-auto my-auto">
-                    <Ripple />
-                  </div>
-                }
-              >
-                <ModuleEdit
-                  user={user}
-                  setModule={setModule}
-                  fetchDrafts={getDraftsAgain}
-                  workspace={currentWorkspace}
-                  module={currentModule}
-                  isAuthor={true}
-                  inboxOpen={inboxOpen}
-                  setInboxOpen={setInboxOpen}
-                  expire={expire}
-                  signature={signature}
-                />
-              </Suspense>
-            ) : (
-              ""
-            )}
-          </div>{" "}
-        </>
-      ) : (
-        <div className="flex flex-col flex-grow relative w-full border-2 border-gray-100 border-dashed rounded-lg text-center focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500  my-4 mx-4">
-          <div className="table flex-grow w-full h-full">
-            <div className="sm:table-cell w-1/4 h-28"></div>
-            <span className="mx-auto table-cell align-middle text-sm leading-4 font-medium">
-              <>
-                <div>No drafts left. Maybe start a new one?</div>
-              </>
-            </span>
-            <div className="hidden sm:table-cell w-1/4"></div>
+        {drafts.length > 0 ? (
+          <>
+            <div
+              className={`${
+                !inboxOpen ? "hidden" : "inline"
+              } float-left w-full lg:w-80 divide-y-0 divide-gray-100 dark:divide-gray-600 overflow-y-auto`}
+              // style={{ minHeight: "calc(100vh - 74px - 54px)" }}
+            >
+              <h1 className="lg:hidden text-lg leading-7 font-medium text-gray-900 dark:text-gray-200 px-4 sm:px-6 lg:px-8 py-4 border-b lg:border-b-0 border-gray-100 dark:border-gray-600">
+                Drafts
+              </h1>
+              <ul role="list" className="divide-y divide-gray-100 dark:divide-gray-600">
+                {drafts.map((draft) => (
+                  <>
+                    <li
+                      onClick={() => {
+                        setModule(draft)
+                        setInboxOpen(biggerWindow)
+                        Router.push("/drafts", { query: { suffix: draft.suffix } })
+                      }}
+                      className="cursor-pointer"
+                    >
+                      <ModuleCard
+                        type={draft.type.name}
+                        title={draft.title}
+                        status="Draft"
+                        time={moment(draft.updatedAt).fromNow()}
+                        timeText="Updated"
+                        authors={draft.authors}
+                      />
+                    </li>
+                  </>
+                ))}
+              </ul>
+            </div>
+            <div
+              className={`${
+                inboxOpen ? "hidden lg:inline" : "inline"
+              } flex-grow w-2/3 float-right overflow-y-auto`}
+            >
+              {currentModule ? (
+                <Suspense
+                  fallback={
+                    <div className="mx-auto my-auto">
+                      <Ripple />
+                    </div>
+                  }
+                >
+                  <ModuleEdit
+                    user={user}
+                    setModule={setModule}
+                    fetchDrafts={getDraftsAgain}
+                    workspace={currentWorkspace}
+                    module={currentModule}
+                    isAuthor={true}
+                    inboxOpen={inboxOpen}
+                    setInboxOpen={setInboxOpen}
+                    expire={expire}
+                    signature={signature}
+                  />
+                </Suspense>
+              ) : (
+                ""
+              )}
+            </div>
+          </>
+        ) : (
+          <div className="flex flex-col flex-grow relative w-full border-2 border-gray-800 dark:border-white border-dashed rounded-lg text-center focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500  my-4 mx-4">
+            <div className="table flex-grow w-full h-full">
+              <div className="sm:table-cell w-1/4 h-28"></div>
+              <span className="mx-auto table-cell align-middle text-sm leading-4 font-medium">
+                <>
+                  <div className="mx-4">No drafts left. Maybe start a new one?</div>
+                </>
+              </span>
+              <div className="hidden sm:table-cell w-1/4"></div>
+            </div>
           </div>
-        </div>
-      )}
-    </div>
+        )}
+      </div>
+    </>
   )
 }
 
