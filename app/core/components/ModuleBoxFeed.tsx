9c7f146456cc979bec33a6322f228c8169434f7d
Chris Hartgerink <chris@libscie.org>
2022-01-31 12:25:51 +0100

    Move button into feed

--- app/core/components/ModuleBoxFeed.tsx
+++ app/core/components/ModuleBoxFeed.tsx
@@ -16,52 +16,54 @@ const ModuleBoxFeed = ({ modules, fetchNextPage, hasNextPage, isFetchingNextPage
           </div>
         </div>
       ) : (
-        modules.map((page, i) => (
-          <>
-            <Fragment key={i}>
-              <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-4 my-4">
-                {page.modules.map((module) => (
-                  <>
+        <>
+          {modules.map((page, i) => (
+            <>
+              <Fragment key={i}>
+                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-4 my-4">
+                  {page.modules.map((module) => (
                     <>
-                      <Link href={Routes.ModulePage({ suffix: module.suffix })}>
-                        <a
-                          className="flex flex-col module cursor-pointer p-4 text-white"
-                          style={{ backgroundColor: module.displayColor }}
-                        >
-                          <h2 className="text-base font-normal leading-5 flex-grow mb-2">
-                            {module.title}
-                          </h2>
-                          <span className="w-full flex">
-                            <span className="flex-grow align-text-bottom text-gray-300"></span>
-                            <AuthorAvatarsNew
-                              authors={module.authors}
-                              size="h-6 w-6"
-                              toDisplay={4}
-                            />
-                          </span>
-                        </a>
-                      </Link>
+                      <>
+                        <Link href={Routes.ModulePage({ suffix: module.suffix })}>
+                          <a
+                            className="flex flex-col module cursor-pointer p-4 text-white"
+                            style={{ backgroundColor: module.displayColor }}
+                          >
+                            <h2 className="text-base font-normal leading-5 flex-grow mb-2">
+                              {module.title}
+                            </h2>
+                            <span className="w-full flex">
+                              <span className="flex-grow align-text-bottom text-gray-300"></span>
+                              <AuthorAvatarsNew
+                                authors={module.authors}
+                                size="h-6 w-6"
+                                toDisplay={4}
+                              />
+                            </span>
+                          </a>
+                        </Link>
+                      </>
                     </>
-                  </>
-                ))}
-              </div>
-            </Fragment>
-          </>
-        ))
+                  ))}
+                </div>
+              </Fragment>
+            </>
+          ))}
+          <div className="text-center my-4">
+            <button
+              onClick={() => fetchNextPage()}
+              disabled={!hasNextPage || !!isFetchingNextPage}
+              className="whitespace-nowrap text-sm leading-5 font-normal text-indigo-700 dark:text-gray-200 bg-indigo-100 hover:bg-indigo-200 dark:bg-gray-800 dark:hover:bg-gray-700 border-0 dark:border dark:border-gray-600 px-4 py-2 rounded focus:outline-none focus:ring-2 focus:ring-offset-0 focus:ring-indigo-500"
+            >
+              {isFetchingNextPage
+                ? "Loading more..."
+                : hasNextPage
+                ? "Load more"
+                : "Nothing more to load"}
+            </button>
+          </div>
+        </>
       )}
-      <div className="text-center my-4">
-        <button
-          onClick={() => fetchNextPage()}
-          disabled={!hasNextPage || !!isFetchingNextPage}
-          className="whitespace-nowrap text-sm leading-5 font-normal text-indigo-700 dark:text-gray-200 bg-indigo-100 hover:bg-indigo-200 dark:bg-gray-800 dark:hover:bg-gray-700 border-0 dark:border dark:border-gray-600 px-4 py-2 rounded focus:outline-none focus:ring-2 focus:ring-offset-0 focus:ring-indigo-500"
-        >
-          {isFetchingNextPage
-            ? "Loading more..."
-            : hasNextPage
-            ? "Load more"
-            : "Nothing more to load"}
-        </button>
-      </div>
     </>
   )
 }
