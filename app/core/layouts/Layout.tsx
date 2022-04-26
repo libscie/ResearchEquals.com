9ce4975d9cf0ae729f8ee023062515e207a105bf
Chris Hartgerink <chris@libscie.org>
2022-04-26 12:40:40 +0200

    Add Crisp upon reload after consent

--- app/core/layouts/Layout.tsx
+++ app/core/layouts/Layout.tsx
@@ -1,4 +1,4 @@
-import { ReactNode, useEffect } from "react"
+import { ReactNode, useEffect, useState } from "react"
 import { Head, Link, Routes } from "blitz"
 import CookieConsent, { Cookies, getCookieConsentValue } from "react-cookie-consent"
 import { Toaster } from "react-hot-toast"
@@ -10,11 +10,15 @@ type LayoutProps = {
   headChildren?: ReactNode
 }
 
+let crispCode = `window.$crisp=[];window.CRISP_WEBSITE_ID="cb17dd6e-f56c-4c2c-a85f-463da113e860";(function(){d=document;s=d.createElement("script");s.src="https://client.crisp.chat/l.js";s.async=1;d.getElementsByTagName("head")[0].appendChild(s);})();`
+
 const Layout = ({ title, children, headChildren }: LayoutProps) => {
-  // useEffect(() => {
-  //   if (getCookieConsentValue("researchequals-website-cookie") === "true") {
-  //   }
-  // }, [])
+  const [cookie, setCookie] = useState(() => <></>)
+  useEffect(() => {
+    if (getCookieConsentValue("researchequals-website-cookie") === "true") {
+      setCookie(<script type="text/javascript">{crispCode}</script>)
+    }
+  }, [])
 
   return (
     <>
@@ -23,6 +27,17 @@ const Layout = ({ title, children, headChildren }: LayoutProps) => {
         <title>{title || "ResearchEquals"}</title>
         <link rel="icon" href="/favicon-32.png" />
         <script data-respect-dnt data-no-cookie async src="https://cdn.splitbee.io/sb.js"></script>
+        {cookie}
+        {/* {cookie ? <script type="text/javascript">{crispCode}</script> : ""} */}
+        {/* <script type="text/javascript">{cookie ? crispCode : false}</script> */}
+        {/* <div
+          dangerouslySetInnerHTML={{
+            __html:
+              getCookieConsentValue("researchequals-website-cookie") !== "true"
+                ? '<script type="text/javascript"></script'
+                : '<script type="text/javascript">window.$crisp=[];window.CRISP_WEBSITE_ID="cb17dd6e-f56c-4c2c-a85f-463da113e860";(function(){d=document;s=d.createElement("script");s.src="https://client.crisp.chat/l.js";s.async=1;d.getElementsByTagName("head")[0].appendChild(s);})();</script>',
+          }}
+        /> */}
         {headChildren}
       </Head>
       <div className="flex min-h-screen flex-col bg-white text-gray-900 dark:bg-gray-900 dark:text-white">
