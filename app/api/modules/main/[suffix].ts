3a5da6b2076a50a2b2783e2c84e9009920419888
Chris Hartgerink <chris@libscie.org>
2022-06-22 15:09:38 +0200

    Add main file api route

--- app/api/modules/main/[suffix].ts
+++ app/api/modules/main/[suffix].ts
@@ -1,10 +1,36 @@
 import { BlitzApiHandler } from "blitz"
+import db from "db"
+import https from "https"
 
-const handler: BlitzApiHandler = (req, res) => {
+const handler: BlitzApiHandler = async (req, res) => {
   const {
     query: { suffix },
   } = req
 
-  res.end(`Post: ${suffix}`)
+  const module = await db.module.findFirst({ where: { suffix: suffix?.toString() } })
+  return new Promise((resolve, reject) => {
+    https
+      .get(module?.main!["cdnUrl"], (response) => {
+        var data = [] as any
+
+        response
+          .on("data", function (chunk) {
+            data.push(chunk)
+          })
+          .on("end", function () {
+            res.statusCode = 200
+            res.setHeader("Content-Type", module?.main!["mimeType"])
+            res.setHeader("Content-Disposition", `filename=${module?.main!["name"]}`)
+            var buffer = Buffer.concat(data)
+            res.end(buffer)
+            resolve()
+          })
+      })
+      .on("error", (e) => {
+        res.status(404).end()
+        resolve()
+      })
+  })
 }
+
 export default handler
