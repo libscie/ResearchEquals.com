6615a150b0842cba4c36798997373d5fdf372d46
Chris Hartgerink <chris@libscie.org>
2022-07-21 09:27:03 +0200

    Add `/api/avatars/[handle]`

---
+++ app/api/avatars/[handle].ts
@@ -0,0 +1,39 @@
+import { BlitzApiHandler } from "blitz"
+import db from "db"
+import https from "https"
+
+const handler: BlitzApiHandler = async (req, res) => {
+  const {
+    query: { handle },
+  } = req
+
+  const author = await db.workspace.findFirst({
+    where: { handle: handle?.toString() },
+  })
+  return new Promise((resolve, reject) => {
+    https
+      .get(author?.avatar!, (response) => {
+        var data = [] as any
+
+        let buffer
+        response
+          .on("data", function (chunk) {
+            data.push(chunk)
+          })
+          .on("end", function () {
+            res.statusCode = 200
+            res.setHeader("Content-Type", "image")
+            res.setHeader("Content-Disposition", `filename=avatar-${author?.handle}`)
+            var buffer = Buffer.concat(data)
+            res.end(buffer)
+            resolve()
+          })
+      })
+      .on("error", (e) => {
+        res.status(404).end()
+        resolve()
+      })
+  })
+}
+
+export default handler
