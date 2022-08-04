731d639ea599f9fb25ad53c8e67dc5189f2851f8
Chris Hartgerink <chris@libscie.org>
2022-08-04 13:59:25 +0200

    Add broadcast mutation

---
+++ app/core/mutations/broadcast-message.ts
@@ -0,0 +1,28 @@
+import { ServerClient, TemplatedMessage } from "postmark"
+import db from "db"
+
+const from = process.env.MAIL_FROM ?? "no-reply@libscie.org"
+
+const postmark = () => new ServerClient(process.env.POSTMARK_TOKEN ?? "")
+
+async function broadcastMessage({ subject, htmlContent, textContent }) {
+  const users = await db.user.findMany({
+    where: {
+      emailIsVerified: true,
+    },
+  })
+
+  await postmark().sendEmailBatch(
+    users.map((user) => {
+      return {
+        From: from,
+        To: user.email,
+        Subject: subject,
+        HtmlBody: htmlContent,
+        TextBody: textContent,
+      }
+    })
+  )
+}
+
+export default broadcastMessage
