50241be16bc9ce5b8d3771c3b47dab542b33e814
Chris Hartgerink <chris@libscie.org>
2022-09-20 21:29:40 +0200

    Add Collection XML generation

---
+++ app/core/crossref/journal.tsx
@@ -0,0 +1,24 @@
+import { Element, Text } from "xast"
+import contributors, { Contributors } from "./contributors"
+import doi_data, { DoiData } from "./doi_data"
+import journal_issue, { JournalIssue, JournalIssueProps } from "./journal_issue"
+import journal_metadata, { JournalMetadata } from "./journal_metadata"
+import publication_date, { PublicationDate } from "./publication_date"
+import titles, { Titles } from "./titles"
+
+export interface Journal extends Element {
+  name: "journal"
+  children: [JournalMetadata, JournalIssue]
+}
+
+const journal = ({ title, subtitle, authors, doi, resolve_url }: JournalIssueProps) => {
+  const js: Journal = {
+    type: "element",
+    name: "journal",
+    children: [journal_metadata(), journal_issue({ title, subtitle, authors, doi, resolve_url })],
+  }
+
+  return js
+}
+
+export default journal
