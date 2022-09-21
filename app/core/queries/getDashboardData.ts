2b402256cedd52fada7b50d38c4b5c35aee4de4f
Chris Hartgerink <chris@libscie.org>
2022-09-21 21:18:08 +0200

    Update query to include editorships'

--- app/core/queries/getDashboardData.ts
+++ app/core/queries/getDashboardData.ts
@@ -25,6 +25,7 @@ export default async function getSignature({ session, changeDays }) {
       followers: true,
       following: true,
       authorships: true,
+      editorships: true,
     },
   })
 
