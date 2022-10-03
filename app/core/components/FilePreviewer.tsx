2a767498d065f3c5058ddd642a3fab9bcbb263cb
Nami Sunami <naoyuki.sunami@gmail.com>
2022-10-03 17:28:03 -0400

    Remove the unused `module` parameter

--- app/core/components/FilePreviewer.tsx
+++ app/core/components/FilePreviewer.tsx
@@ -11,7 +11,7 @@ import rehypeKatex from "rehype-katex"
 import "katex/dist/katex.min.css" // `rehype-katex` does not import the CSS for you
 import { useEffect, useState } from "react"
 
-const MainFileViewer = ({ mainFile, module }) => {
+const MainFileViewer = ({ mainFile }) => {
   const prefersDarkMode = useMediaPredicate("(prefers-color-scheme: dark)")
   const [mainFileMarkdown, setMarkdown] = useState("")
 
