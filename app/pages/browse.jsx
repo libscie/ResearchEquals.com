99b0eabaf114a6d8d88567755656b4be00387321
Nami Sunami <naoyuki.sunami@gmail.com>
2022-10-05 16:46:28 -0400

    Add key props to `BrowseWorkspaces`

--- app/pages/browse.jsx
+++ app/pages/browse.jsx
@@ -49,24 +49,20 @@ const BrowseWorkspaces = () => {
         <React.Fragment key={i}>
           <div className="my-4 grid grid-cols-2 gap-4 gap-y-10 md:grid-cols-3 xl:grid-cols-4">
             {page.workspaces.map((workspace) => (
-              <>
-                <>
-                  <Link href={Routes.HandlePage({ handle: workspace.handle })}>
-                    <a className="mx-auto text-center">
-                      <img
-                        src={workspace.avatar}
-                        alt={`Avatar of ${workspace.handle}`}
-                        className="mx-auto h-28 w-28 rounded-full"
-                      />
-                      <p className="mx-auto my-2 text-center">
-                        {workspace.firstName && workspace.lastName
-                          ? `${workspace.firstName} ${workspace.lastName}`
-                          : `@${workspace.handle}`}{" "}
-                      </p>
-                    </a>
-                  </Link>
-                </>
-              </>
+              <Link key={workspace.id} href={Routes.HandlePage({ handle: workspace.handle })}>
+                <a className="mx-auto text-center">
+                  <img
+                    src={workspace.avatar}
+                    alt={`Avatar of ${workspace.handle}`}
+                    className="mx-auto h-28 w-28 rounded-full"
+                  />
+                  <p className="mx-auto my-2 text-center">
+                    {workspace.firstName && workspace.lastName
+                      ? `${workspace.firstName} ${workspace.lastName}`
+                      : `@${workspace.handle}`}{" "}
+                  </p>
+                </a>
+              </Link>
             ))}
           </div>
         </React.Fragment>
