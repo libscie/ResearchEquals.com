cc1b5326a9a67c68e96225d540cf3629f291e1b1
Chris Hartgerink <chris@libscie.org>
2022-10-24 17:04:46 +0200

    Add logic to not show contributors badge if 0

--- app/collections/components/ContributorsBadge.tsx
+++ app/collections/components/ContributorsBadge.tsx
@@ -14,14 +14,16 @@ const ContributorsBadge = ({ collection, nrContributors }) => {
 
   return (
     <>
-      {JSON.stringify(collection) != "{}" ? (
+      {JSON.stringify(collection) != "{}" && submissionId.filter(onlyUnique).length > 0 && (
         <span
           className={`m-1 inline-flex items-center rounded-full bg-cyan-100 px-3 py-0.5 text-sm font-medium text-cyan-800`}
         >
           {submissionId.filter(onlyUnique).length} contributor
           {submissionId.filter(onlyUnique).length != 1 && "s"}
         </span>
-      ) : (
+      )}
+
+      {JSON.stringify(nrContributors) != "{}" && nrContributors > 0 && (
         <span
           className={`m-1 inline-flex items-center rounded-full bg-cyan-100 px-3 py-0.5 text-sm font-medium text-cyan-800`}
         >
