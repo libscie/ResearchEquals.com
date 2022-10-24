12a08d5f133bba13bf4b1d104b6da40cf71d55dc
Chris Hartgerink <chris@libscie.org>
2022-10-24 16:30:00 +0200

    Improve `my` on collection badges

--- app/collections/components/ContributorsBadge.tsx
+++ app/collections/components/ContributorsBadge.tsx
@@ -16,14 +16,14 @@ const ContributorsBadge = ({ collection, nrContributors }) => {
     <>
       {JSON.stringify(collection) != "{}" ? (
         <span
-          className={`mx-1 inline-flex items-center rounded-full bg-cyan-100 px-3 py-0.5 text-sm font-medium text-cyan-800`}
+          className={`m-1 inline-flex items-center rounded-full bg-cyan-100 px-3 py-0.5 text-sm font-medium text-cyan-800`}
         >
           {submissionId.filter(onlyUnique).length} contributor
           {submissionId.filter(onlyUnique).length != 1 && "s"}
         </span>
       ) : (
         <span
-          className={`mx-1 inline-flex items-center rounded-full bg-cyan-100 px-3 py-0.5 text-sm font-medium text-cyan-800`}
+          className={`m-1 inline-flex items-center rounded-full bg-cyan-100 px-3 py-0.5 text-sm font-medium text-cyan-800`}
         >
           {nrContributors} contributor
           {nrContributors != 1 && "s"}
