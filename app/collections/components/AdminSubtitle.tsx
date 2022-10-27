17576b9a179e8c8f96cda05759e6d75bbe57cdd5
Chris Hartgerink <chris@libscie.org>
2022-10-27 16:41:00 +0200

    Refactor to have save buttons

--- app/collections/components/AdminSubtitle.tsx
+++ app/collections/components/AdminSubtitle.tsx
@@ -1,54 +1,64 @@
-import { useMutation } from "blitz"
+import { useMutation, validateZodSchema } from "blitz"
 import { Field, Form, Formik } from "formik"
 import toast from "react-hot-toast"
 import changeSubtitle from "../mutations/changeSubtitle"
+import { useFormik } from "formik"
+import { z } from "zod"
 
 const Subtitle = ({ collection, refetchFn, isAdmin }) => {
   const [changeSubtitleMutation, { isSuccess: isSubtitleSuccess }] = useMutation(changeSubtitle)
+
+  const formik = useFormik({
+    initialValues: {
+      subtitle: collection.subtitle,
+    },
+    validate: validateZodSchema(
+      z.object({
+        subtitle: z.string().min(1).max(300),
+      })
+    ),
+    onSubmit: async (values) => {
+      toast.promise(
+        changeSubtitleMutation({
+          id: collection.id,
+          subtitle: values.subtitle,
+        }),
+        {
+          loading: "Updating subtitle...",
+          success: () => {
+            refetchFn()
+            return "Updated subtitle"
+          },
+          error: "Failed to update subtitle",
+        }
+      )
+    },
+  })
 
   return (
     <>
       {(collection.subtitle === null || !collection.public || collection.upgraded) && isAdmin ? (
         <>
-          <Formik
-            initialValues={{
-              subtitle: collection.subtitle,
-            }}
-            onSubmit={() => {}}
+          <form
+            className="module my-2 bg-white pb-2 dark:border-gray-600 dark:bg-gray-900"
+            onSubmit={formik.handleSubmit}
           >
-            <Form
-              className="my-4"
-              onBlur={(values) => {
-                if (collection.subtitle != values.target.defaultValue) {
-                  toast.promise(
-                    changeSubtitleMutation({
-                      id: collection.id,
-                      subtitle: values.target.defaultValue,
-                    }),
-                    {
-                      loading: "Updating subtitle...",
-                      success: () => {
-                        refetchFn()
-                        return "Updated subtitle"
-                      },
-                      error: "Failed to update subtitle",
-                    }
-                  )
-                }
-              }}
+            <label htmlFor="subtitle" className="sr-only">
+              subtitle
+            </label>
+            <textarea
+              rows={1}
+              id="subtitle"
+              className="w-full border-0 text-center text-sm font-medium leading-5 focus:ring-0 dark:bg-gray-900 md:text-base "
+              {...formik.getFieldProps("subtitle")}
+            />
+            <button
+              type="submit"
+              className="mx-auto my-1 flex rounded-md bg-emerald-50 py-2 px-4 text-sm font-medium text-emerald-700 hover:bg-emerald-200 focus:outline-none focus:ring-2 focus:ring-emerald-500 focus:ring-offset-0 dark:border dark:border-gray-600 dark:bg-gray-800 dark:text-emerald-500 dark:hover:border-gray-400 dark:hover:bg-gray-700"
             >
-              <label htmlFor="subtitle" className="sr-only">
-                subtitle
-              </label>
-              <Field
-                id="subtitle"
-                name="subtitle"
-                placeholder={collection.subtitle}
-                type="text"
-                className="w-full border-0 text-center text-sm font-medium leading-5 focus:ring-0 dark:bg-gray-900 md:text-base "
-              />
-            </Form>
-          </Formik>
+              Save Subtitle
+            </button>
+          </form>
         </>
       ) : (
         <h2 className="mx-auto text-center text-sm font-medium leading-5 md:text-base">
