1ff76309ad6b006ff1c7655aae79c85ce4eb7087
Nami Sunami <naoyuki.sunami@gmail.com>
2022-10-30 18:53:57 +0100

    Componentize the hero section & button

---
+++ app/core/components/home/Hero.tsx
@@ -0,0 +1,33 @@
+import { useRouter } from "blitz"
+import React, { useEffect } from "react"
+import { useTranslation } from "react-i18next"
+import Button from "./Button"
+import "app/core/i18n"
+
+export const Hero = () => {
+  const { t, i18n } = useTranslation()
+  const router = useRouter()
+
+  useEffect(() => {
+    i18n.changeLanguage(router.locale)
+  }, [])
+
+  return (
+    <section id="hero" className="bg-indigo-50 py-10 dark:bg-transparent md:py-28 md:text-center">
+      <div className="flex flex-col items-center gap-4 px-7">
+        <h1 className="max-w-5xl text-4xl font-bold text-slate-800 dark:text-white md:text-7xl md:font-extrabold">
+          {t("hero")}
+        </h1>
+        <p className="max-w-[800px] text-base text-slate-600 dark:text-slate-300 md:text-xl">
+          {t("hero_subtitle")}
+        </p>
+        {/* TODO CTA_LINK */}
+        {/* <Link href="<CTA_LINK>" passHref> */}
+        <Button color="primary" className="mt-4 w-auto" onClick={() => router.push("/browse")}>
+          Give Research Equals a Try
+        </Button>
+        {/* </Link> */}
+      </div>
+    </section>
+  )
+}
