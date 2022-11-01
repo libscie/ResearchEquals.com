d201e9860990205f1a775168a50954df410e84c1
Nami Sunami <nsunami@pm.me>
2022-11-01 13:36:14 +0100

    Revise the primary action text
    Co-authored-by: Chris Hartgerink <chris@libscie.org>

--- app/collections/components/AdminEditors.tsx
+++ app/collections/components/AdminEditors.tsx
@@ -75,7 +75,7 @@ const EditorCard = ({ editor, isAdmin, isSelf, refetchFn }) => {
                   collection? By doing so, you will lose access to the admin panel.
                 </span>
               }
-              primaryAction="Assign yourself as a user"
+              primaryAction="Change Role"
               primaryButtonClass="rounded-md bg-red-50 py-2 px-4 text-sm font-medium text-red-700 hover:bg-red-200 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-0 dark:border dark:border-gray-600 dark:bg-gray-800 dark:text-red-500 dark:hover:border-gray-400 dark:hover:bg-gray-700"
               isOpen={isConfirmOpen}
               setIsOpen={setIsConfirmOpen}
