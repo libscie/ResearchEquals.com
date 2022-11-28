41ebff464a9963b5c90b5d8380bbf49c8e49857e
Nami Sunami <naoyuki.sunami@gmail.com>
2022-11-28 09:50:08 +0100

    Await and unnest promise in toast in Collection Payment Modal

--- app/core/modals/PayCreateCollectionModal.tsx
+++ app/core/modals/PayCreateCollectionModal.tsx
@@ -93,19 +93,18 @@ export default function PayCreateCollectionModal({ user, price, type, workspace 
                           type="submit"
                           role="link"
                           className="mr-2 inline-flex justify-center rounded-md bg-emerald-50 py-2 px-4 text-sm font-medium text-emerald-700 hover:bg-emerald-200 focus:outline-none focus:ring-2 focus:ring-emerald-500 focus:ring-offset-0 disabled:cursor-not-allowed disabled:opacity-50 dark:border dark:border-gray-600 dark:bg-gray-800 dark:text-emerald-500 dark:hover:border-gray-400 dark:hover:bg-gray-700"
-                          onClick={() => {
-                            toast
+                          onClick={async () => {
+                            await toast
                               .promise(createIndividualCollectionMutation({}), {
                                 loading: "Creating collection...",
-                                success: (data) => {
-                                  router.push(`/collections/${data}/admin`).catch(() => {})
-                                  return "Created collection!"
-                                },
+                                success: "Created collection!",
                                 error: (err) => {
                                   return err.toString()
                                 },
                               })
-                              .catch(() => {})
+                              .then(async (data) => {
+                                await router.push(`/collections/${data}/admin`)
+                              })
                             closeModal()
                             // router.push("/collections")
                           }}
