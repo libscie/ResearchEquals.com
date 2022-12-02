1e185838b5926de5c8a9157d1c86fcceeec007b9
Nami Sunami <naoyuki.sunami@gmail.com>
2022-12-02 13:37:55 +0100

    Add `key` props in `WhoToFollow`

--- app/core/components/WhoToFollow.tsx
+++ app/core/components/WhoToFollow.tsx
@@ -12,31 +12,29 @@ const WhoToFollow = ({ data, workspace, refetch, refetchFeed }) => {
     <>
       <h2 className="my-2 text-2xl font-medium">Who to follow</h2>
       {data.followableWorkspaces.map((author) => (
-        <>
-          <li className="flex py-2">
-            <div className="mr-2">
-              <img
-                src={author.avatar}
-                alt={`Avatar of ${author.handle}`}
-                className="inline-block h-10 h-full w-10 rounded-full align-middle"
-              />
-            </div>
-            <Link href={Routes.HandlePage({ handle: author.handle })} className="flex-grow">
-              <span className="inline-block h-full align-middle"></span>
-              <p className="my-auto inline-block align-middle text-sm font-normal leading-4 text-gray-700 dark:text-gray-200">
-                {author.firstName} {author.lastName}
-                <p className="text-xs font-normal leading-4 text-gray-500 dark:text-gray-400">
-                  @{author.handle}
-                </p>
+        <li key={author.id} className="flex py-2">
+          <div className="mr-2">
+            <img
+              src={author.avatar}
+              alt={`Avatar of ${author.handle}`}
+              className="inline-block h-10 h-full w-10 rounded-full align-middle"
+            />
+          </div>
+          <Link href={Routes.HandlePage({ handle: author.handle })} className="flex-grow">
+            <span className="inline-block h-full align-middle"></span>
+            <p className="my-auto inline-block align-middle text-sm font-normal leading-4 text-gray-700 dark:text-gray-200">
+              {author.firstName} {author.lastName}
+              <p className="text-xs font-normal leading-4 text-gray-500 dark:text-gray-400">
+                @{author.handle}
               </p>
-            </Link>
-            {workspace!.following.filter((follow) => follow.handle === author.handle).length > 0 ? (
-              <UnfollowButton author={author} refetchFn={refetchFn} />
-            ) : (
-              <FollowButton author={author} refetchFn={refetchFn} />
-            )}
-          </li>
-        </>
+            </p>
+          </Link>
+          {workspace!.following.filter((follow) => follow.handle === author.handle).length > 0 ? (
+            <UnfollowButton author={author} refetchFn={refetchFn} />
+          ) : (
+            <FollowButton author={author} refetchFn={refetchFn} />
+          )}
+        </li>
       ))}
       <div className="my-4 text-center">
         <Link href="/browse" legacyBehavior>
