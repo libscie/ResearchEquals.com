8064c6e61f09d0a9d21ceeed33e055ca4731b3cc
Chris Hartgerink <chris@libscie.org>
2023-02-28 18:37:35 +0100

    Simplify getCurrentEvent

--- pages/membership-area/[slug].tsx
+++ pages/membership-area/[slug].tsx
@@ -1,44 +1,28 @@
-import { gSSP } from "app/blitz-server"
 import { useSession } from "@blitzjs/auth"
 import { useRouter } from "next/router"
 import { useQuery } from "@blitzjs/rpc"
+import getCurrentEvent from "../../app/core/queries/getCurrentEvent"
 import "quill/dist/quill.snow.css"
 
 import Layout from "app/core/layouts/Layout"
-import db from "db"
 import Navbar from "app/core/components/Navbar"
 import LayoutLoader from "app/core/components/LayoutLoader"
 import getInvitedModules from "app/workspaces/queries/getInvitedModules"
 import getDrafts from "app/core/queries/getDrafts"
 import { useCurrentWorkspace } from "app/core/hooks/useCurrentWorkspace"
 import { useCurrentUser } from "app/core/hooks/useCurrentUser"
-import { NotFoundError } from "blitz"
 
-export const getServerSideProps = gSSP(async function getServerSideProps(context) {
-  const currentEvent = await db.supportingEvents.findFirst({
-    where: {
-      slug: context?.params?.slug?.toString().toLowerCase(),
-    },
-  })
-
-  if (!currentEvent) throw new NotFoundError()
-  return {
-    props: {
-      currentEvent,
-    },
-  }
-})
-
-const SupportingEventPage = ({ currentEvent }) => {
+const SupportingEventPage = () => {
   const currentUser = useCurrentUser()
   const session = useSession()
   const currentWorkspace = useCurrentWorkspace()
   const router = useRouter()
   const [drafts, { refetch }] = useQuery(getDrafts, { session })
+  const [currentEvent] = useQuery(getCurrentEvent, "ga-1")
   const [invitations] = useQuery(getInvitedModules, { session })
 
   return (
-    <Layout title={`${currentEvent.title} | Membership Area`}>
+    <Layout title={`${currentEvent!.title} | Membership Area`}>
       <Navbar
         currentUser={currentUser}
         session={session}
@@ -50,10 +34,10 @@ const SupportingEventPage = ({ currentEvent }) => {
       />
       <main className="bg-white dark:bg-gray-900 lg:relative">
         <div className="mx-4 my-8 max-w-3xl text-black dark:text-white lg:mx-auto">
-          <h1 className="mx-auto my-8 text-center text-5xl font-black">{currentEvent.title}</h1>
+          <h1 className="mx-auto my-8 text-center text-5xl font-black">{currentEvent!.title}</h1>
           <div
             className="coc ql-editor"
-            dangerouslySetInnerHTML={{ __html: currentEvent.content }}
+            dangerouslySetInnerHTML={{ __html: currentEvent!.content }}
           ></div>
         </div>
       </main>
@@ -61,6 +45,7 @@ const SupportingEventPage = ({ currentEvent }) => {
   )
 }
 
+SupportingEventPage.authenticate = { role: ["SUPERADMIN", "SUPPORTING"] }
 SupportingEventPage.suppressFirstRenderFlicker = true
 SupportingEventPage.getLayout = (page) => <LayoutLoader>{page}</LayoutLoader>
 
