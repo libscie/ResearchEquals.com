334e299dc23cf4a4838d375dd343a3cd87cd7cfe
Chris Hartgerink <chris@libscie.org>
2023-04-26 16:59:30 +0200

    Start preparing institutional supporting members

--- app/core/components/InstitutionalSupportingMembers.tsx
+++ app/core/components/InstitutionalSupportingMembers.tsx
@@ -1,3 +1,5 @@
+import Tiu from "../icons/tiu"
+
 const InstitutionalSupportingMembers = () => {
   return (
     <div className="bg-white py-24 dark:bg-gray-900 sm:py-24">
@@ -5,7 +7,8 @@ const InstitutionalSupportingMembers = () => {
         <h3 className="my-8 text-center text-lg font-bold text-slate-800 dark:text-white md:text-2xl">
           Institutional supporting members
         </h3>
-        <div className="mx-auto grid max-w-lg grid-cols-4 items-center gap-x-8 gap-y-12 py-8 dark:invert sm:max-w-xl sm:grid-cols-6 sm:gap-x-10 sm:gap-y-14 lg:mx-0 lg:max-w-none lg:grid-cols-5 ">
+        <div className="mx-auto grid max-w-lg grid-cols-3 items-center gap-x-8 gap-y-12 py-8 sm:max-w-xl sm:grid-cols-4 sm:gap-x-10 sm:gap-y-14 lg:mx-0 lg:max-w-none">
+          <Tiu />
           <img
             className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
             src="https://tailwindui.com/img/logos/158x48/transistor-logo-gray-900.svg"
