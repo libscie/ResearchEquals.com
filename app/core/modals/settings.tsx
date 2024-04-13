2534d2cf83f7928166923bd906bed62435cf4319
Nami Sunami <nsunami@pm.me>
2024-04-13 19:38:54 +0200

    Removing unused `settingsModalAtom` import

--- app/core/modals/settings.tsx
+++ app/core/modals/settings.tsx
@@ -2,7 +2,7 @@ import { Dialog, Transition, Tab } from "@headlessui/react"
 import { Fragment, useEffect, useState } from "react"
 import { Close } from "@carbon/icons-react"
 import { useRecoilState } from "recoil"
-import { emailNotificationsAtom, settingsModalAtom } from "../utils/Atoms"
+import { emailNotificationsAtom } from "../utils/Atoms"
 
 import WorkspaceSettings from "../components/WorkspaceSettings"
 import AccountSettings from "../components/AccountSettings"
